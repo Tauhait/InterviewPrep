790b2ef6677bf4f05aed11eff338728bfa33dc4c
Tuhin Dutta <dt_tauhait@hotmail.com>
2020-10-31 11:28:40 +0530

    Updated 31st Oct, 2020

--- Coding/src/faang/Tester.java
+++ Coding/src/faang/Tester.java
@@ -3,8 +3,10 @@ package faang;
 public class Tester {
 
 	public static void main(String[] args) {
-		AsteroidCollision obj = new AsteroidCollision();
-		int[] arr = {};
+		MinimumKnightMoves obj = new MinimumKnightMoves();
+		int moves = obj.getMinMoves(4, 4, 4, 5, 8);
+		if(moves < 0) System.out.println("Invalid starting knight position or target point");
+		else System.out.printf("Min moves from (%d, %d) to (%d, %d) is = %d", 4, 4, 4, 5, moves);
 	}
 
 }
