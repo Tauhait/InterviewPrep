c5238c1e8393cd52c6fa2790f6c1b2948886de2a
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-01-28 15:39:01 +0530

    Time: 23 ms (7.28%), Space: 48.7 MB (5.18%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,6 +8,56 @@
  * }
  */
 class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+        Map<TreeNode, TreeNode> parent = new HashMap<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        parent.put(root, null);
+        stack.push(root);
+
+        while (!parent.containsKey(p) || !parent.containsKey(q)) {
+            TreeNode node = stack.pop();
+            if (node.left != null) {
+                parent.put(node.left, node);
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                parent.put(node.right, node);
+                stack.push(node.right);
+            }
+        }
+        Set<TreeNode> ancestors = new HashSet<>();
+        while (p != null) {
+            ancestors.add(p);
+            p = parent.get(p);
+        }
+        /*
+        Important code below!!
+        */
+        while (!ancestors.contains(q)){
+            q = parent.get(q);
+        }
+        
+        return q;
+    }
+    /*
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q){
+            return root;            
+        }
+        
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        if(left == null && right == null){
+            return null;
+        }  
+        if(left != null && right != null){
+            return root;
+        }
+        return left == null ? right : left;
+    }
+    */
+    /*
     LinkedList<TreeNode> route;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         TreeNode answer = null;
@@ -53,4 +103,5 @@ class Solution {
         route.remove(route.size() - 1);
         return false;
     }
+    */
 }
