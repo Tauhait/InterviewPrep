264ce6833e795b29a0c4f98b4facf48be8a9aa6e
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-01-28 15:24:05 +0530

    Time: 5 ms (62.36%), Space: 43.8 MB (49.25%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,6 +8,23 @@
  * }
  */
 class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q){
+            return root;            
+        }
+        
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        if(left == null && right == null){
+            return null;
+        }  
+        if(left != null && right != null){
+            return root;
+        }
+        return left == null ? right : left;
+    }
+    /*
     LinkedList<TreeNode> route;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         TreeNode answer = null;
@@ -53,4 +70,5 @@ class Solution {
         route.remove(route.size() - 1);
         return false;
     }
+    */
 }
