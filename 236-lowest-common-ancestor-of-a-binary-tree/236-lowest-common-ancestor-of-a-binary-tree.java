ef11832bc7174a79c3e005ec2bcf096a94d1b8a0
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-01-28 15:14:54 +0530

    Time: 247 ms (5.02%), Space: 50.2 MB (5.18%) - LeetHub

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    LinkedList<TreeNode> route;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode answer = null;
+        route = new LinkedList<TreeNode>();
+        LinkedList<TreeNode> routeQ = null;
+        LinkedList<TreeNode> routeP = null;
+        if(findRoute(root, p)){
+            routeP = (LinkedList)route.clone();
+        }
+        
+        route = new LinkedList<TreeNode>();
+        
+        if(findRoute(root, q)){
+            routeQ = (LinkedList)route.clone();
+        }
+        if(routeP == null || routeQ == null){
+            return answer;
+        }
+        
+        int idx = 0;
+        while(idx < routeP.size() && idx < routeQ.size()){
+            if(routeP.get(idx).val == routeQ.get(idx).val){
+                answer = routeP.get(idx);
+            }
+            idx++;
+        }
+        
+        return answer;
+        
+    }
+    private boolean findRoute(TreeNode root, TreeNode node){
+        if(root == null){
+            return false;
+        }
+        route.add(root);        
+        if(root.val == node.val){
+            return true;
+        }
+              
+        if(findRoute(root.left, node) || findRoute(root.right, node)){
+            return true;
+        } 
+        route.remove(route.size() - 1);
+        return false;
+    }
+}
