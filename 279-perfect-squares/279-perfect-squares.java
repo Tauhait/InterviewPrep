8cc2ea4b0d6de1f7100ded8f38a1e3f6ef1eee8d
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-02-09 18:43:56 +0530

    Time: 89 ms (38.43%), Space: 56.9 MB (17.21%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.java
+++ 279-perfect-squares/279-perfect-squares.java
@@ -1,34 +1,32 @@
 class Solution {
-    public int numSquares(int n) {
-        Queue<Integer> perfectSqQueue = new LinkedList();
-        Queue<Integer> intermediateN = new LinkedList();
-        int sqrootN = (int)Math.sqrt(n);
-        for(int i = 1; i <= sqrootN; i++){
-            int squareOfI = i * i;
-            perfectSqQueue.add(squareOfI);
-        }
-        intermediateN.offer(n); 
-        int depth = 0;
-        while(!intermediateN.isEmpty()){
-            int size = intermediateN.size();  
-            while(size-- > 0){
-                Integer newN = intermediateN.poll();
-                Iterator<Integer> perfectQIter = perfectSqQueue.iterator();
-                while(perfectQIter.hasNext()){
-                    Integer sq = perfectQIter.next();
-                    //System.out.print(sq + " ");
-                    if(newN.equals(sq)){
-                        return depth + 1;
-                    }else if(newN < sq){
-                        break;
-                    }else {
-                        intermediateN.offer(newN - sq);
-                    }                   
-                }
-                //System.out.println();
-            }
-            depth++;
-        }
-        return depth;
+  public int numSquares(int n) {
+
+    ArrayList<Integer> square_nums = new ArrayList<Integer>();
+    for (int i = 1; i * i <= n; ++i) {
+      square_nums.add(i * i);
     }
+
+    Set<Integer> queue = new HashSet<Integer>();
+    queue.add(n);
+
+    int level = 0;
+    while (queue.size() > 0) {
+      level += 1;
+      Set<Integer> next_queue = new HashSet<Integer>();
+
+      for (Integer remainder : queue) {
+        for (Integer square : square_nums) {
+          if (remainder.equals(square)) {
+            return level;
+          } else if (remainder < square) {
+            break;
+          } else {
+            next_queue.add(remainder - square);
+          }
+        }
+      }
+      queue = next_queue;
+    }
+    return level;
+  }
 }
