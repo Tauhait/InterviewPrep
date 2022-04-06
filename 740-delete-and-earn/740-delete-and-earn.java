6467e7ac9d026c14d22bc463541fbcdb87d2faae
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-04-06 09:25:50 +0530

    Time: 8 ms (39.47%), Space: 43.2 MB (81.17%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,21 +1,30 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
-        int maxNumber = 0;
         HashMap<Integer, Integer> points = new HashMap<>();
         
         // Precompute how many points we gain from taking an element
         for (int num : nums) {
             points.put(num, points.getOrDefault(num, 0) + num);
-            maxNumber = Math.max(maxNumber, num);
         }
+        
+        List<Integer> elements = new ArrayList<Integer>(points.keySet());
+        Collections.sort(elements);
         
         // Base cases
         int twoBack = 0;
-        int oneBack = points.getOrDefault(1, 0);
+        int oneBack = points.get(elements.get(0));
         
-        for (int num = 2; num <= maxNumber; num++) {
+        for (int i = 1; i < elements.size(); i++) {
+            int currentElement = elements.get(i);
             int temp = oneBack;
-            oneBack = Math.max(oneBack, twoBack + points.getOrDefault(num, 0));
+            if (currentElement == elements.get(i - 1) + 1) {
+                // The 2 elements are adjacent, cannot take both - apply normal recurrence
+                oneBack = Math.max(oneBack, twoBack + points.get(currentElement));
+            } else {
+                // Otherwise, we don't need to worry about adjacent deletions
+                oneBack += points.get(currentElement);
+            }
+
             twoBack = temp;
         }
         
