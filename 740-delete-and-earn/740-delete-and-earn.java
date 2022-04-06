bc92f19390c9074c8a16e6bdff23e8355b11fd42
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-04-06 09:12:35 +0530

    Time: 13 ms (17.96%), Space: 47.9 MB (15.97%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,28 +1,6 @@
 class Solution {
-    private HashMap<Integer, Integer> points = new HashMap<>();
-    private HashMap<Integer, Integer> cache = new HashMap<>();
-    
-    private int maxPoints(int num) {
-        // Check for base cases
-        if (num == 0) {
-            return 0;
-        }
-        
-        if (num == 1) {
-            return points.getOrDefault(1, 0);
-        }
-        
-        if (cache.containsKey(num)) {
-            return cache.get(num);
-        }
-        
-        // Apply recurrence relation
-        int gain = points.getOrDefault(num, 0);
-        cache.put(num, Math.max(maxPoints(num - 1), maxPoints(num - 2) + gain));
-        return cache.get(num);
-    }
-    
     public int deleteAndEarn(int[] nums) {
+        HashMap<Integer, Integer> points = new HashMap<>();
         int maxNumber = 0;
         
         // Precompute how many points we gain from taking an element
@@ -31,6 +9,16 @@ class Solution {
             maxNumber = Math.max(maxNumber, num);
         }
         
-        return maxPoints(maxNumber);
+        // Declare our array along with base cases
+        int[] maxPoints = new int[maxNumber + 1];
+        maxPoints[1] = points.getOrDefault(1, 0);
+        
+        for (int num = 2; num < maxPoints.length; num++) {
+            // Apply recurrence relation
+            int gain = points.getOrDefault(num, 0);
+            maxPoints[num] = Math.max(maxPoints[num - 1], maxPoints[num - 2] + gain);
+        }
+        
+        return maxPoints[maxNumber];
     }
 }
