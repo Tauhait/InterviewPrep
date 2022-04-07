6daf0c8fab94431155181dae5c3881a9cde8f6c3
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-04-07 11:10:12 +0530

    Time: 297 ms, Memory: 120 MB - LeetHub

---
+++ maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.java
@@ -0,0 +1,31 @@
+class Solution {
+    private int[][] memo;
+    private int[] nums, multipliers;
+    private int n, m;
+    
+    private int dp(int i, int left) {
+        if (i == m) {
+            return 0; // Base case
+        }
+
+        int mult = multipliers[i];
+        int right = n - 1 - (i - left);
+            
+        if (memo[i][left] == 0) {
+            // Recurrence relation
+            memo[i][left] = Math.max(mult * nums[left] + dp(i + 1, left + 1), 
+                                     mult * nums[right] + dp(i + 1, left));
+        }
+
+        return memo[i][left];
+    }
+    
+    public int maximumScore(int[] nums, int[] multipliers) {
+        n = nums.length;
+        m = multipliers.length;
+        this.nums = nums;
+        this.multipliers = multipliers;
+        this.memo = new int[m][m];
+        return dp(0, 0);
+    }
+}
