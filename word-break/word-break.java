a0167f1c757071b8efb20ffce980495d52eae201
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-04-15 12:54:00 +0530

    Time: 3 ms, Memory: 40.7 MB - LeetHub

---
+++ word-break/word-break.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            for (String word : wordDict) {
+                // Make sure to stay in bounds while checking criteria
+                if (i >= word.length() - 1 && (i == word.length() - 1 || dp[i - word.length()])) {
+                    if (s.substring(i - word.length() + 1, i + 1).equals(word)) {
+                        dp[i] = true;   
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return dp[s.length() - 1];
+    }
+}
