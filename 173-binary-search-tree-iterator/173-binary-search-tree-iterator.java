2faaa2b838a11150c49288f59f661935eb70ff04
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-04-21 00:00:23 +0530

    Time: 16 ms (77.71%), Space: 46 MB (92.04%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.java
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.java
@@ -14,36 +14,49 @@
  * }
  */
 class BSTIterator {
-    List<Integer> inorderList;
-    int index;
-    
+
+    Stack<TreeNode> stack;
+
     public BSTIterator(TreeNode root) {
-        inorderList = new ArrayList<Integer>();
-        index = 0;
-        inorder(root);
+
+        // Stack for the recursion simulation
+        this.stack = new Stack<TreeNode>();
+
+        // Remember that the algorithm starts with a call to the helper function
+        // with the root node as the input
+        this._leftmostInorder(root);
     }
-    
-    private void inorder(TreeNode root){
-        if(root != null){
-            inorder(root.left);
-            inorderList.add(root.val);
-            inorder(root.right);
+
+    private void _leftmostInorder(TreeNode root) {
+
+        // For a given node, add all the elements in the leftmost branch of the tree
+        // under it to the stack.
+        while (root != null) {
+            this.stack.push(root);
+            root = root.left;
         }
     }
-    
+
+    /**
+     * @return the next smallest number
+     */
     public int next() {
-        int ans = inorderList.get(index++);
-        return ans;
+        // Node at the top of the stack is the next smallest element
+        TreeNode topmostNode = this.stack.pop();
+
+        // Need to maintain the invariant. If the node has a right child, call the
+        // helper function for the right child
+        if (topmostNode.right != null) {
+            this._leftmostInorder(topmostNode.right);
+        }
+
+        return topmostNode.val;
     }
-    
+
+    /**
+     * @return whether we have a next smallest number
+     */
     public boolean hasNext() {
-        return index < inorderList.size();
+        return this.stack.size() > 0;
     }
 }
-
-/**
- * Your BSTIterator object will be instantiated and called as such:
- * BSTIterator obj = new BSTIterator(root);
- * int param_1 = obj.next();
- * boolean param_2 = obj.hasNext();
- */
