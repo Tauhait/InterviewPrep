ae7766c3fcddf1e9df8d34e4e6e01f16a0c8a65c
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-04-23 00:43:15 +0530

    Time: 32 ms (53.08%), Space: 63.9 MB (42.29%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.java
+++ 706-design-hashmap/706-design-hashmap.java
@@ -1,87 +1,99 @@
-class Pair<U, V> {
-  public U first;
-  public V second;
-
-  public Pair(U first, V second) {
-    this.first = first;
-    this.second = second;
-  }
-}
-
-
-class Bucket {
-  private List<Pair<Integer, Integer>> bucket;
-
-  public Bucket() {
-    this.bucket = new LinkedList<Pair<Integer, Integer>>();
-  }
-
-  public Integer get(Integer key) {
-    for (Pair<Integer, Integer> pair : this.bucket) {
-      if (pair.first.equals(key))
-        return pair.second;
-    }
-    return -1;
-  }
-
-  public void update(Integer key, Integer value) {
-    boolean found = false;
-    for (Pair<Integer, Integer> pair : this.bucket) {
-      if (pair.first.equals(key)) {
-        pair.second = value;
-        found = true;
-      }
-    }
-    if (!found)
-      this.bucket.add(new Pair<Integer, Integer>(key, value));
-  }
-
-  public void remove(Integer key) {
-    for (Pair<Integer, Integer> pair : this.bucket) {
-      if (pair.first.equals(key)) {
-        this.bucket.remove(pair);
-        break;
-      }
-    }
-  }
-}
-
 class MyHashMap {
-  private int key_space;
-  private List<Bucket> hash_table;
-
-  /** Initialize your data structure here. */
-  public MyHashMap() {
-    this.key_space = 2069;
-    this.hash_table = new ArrayList<Bucket>();
-    for (int i = 0; i < this.key_space; ++i) {
-      this.hash_table.add(new Bucket());
+    private Bucket hMap[];
+    private final int buckets = 2069;
+    public MyHashMap() {        
+        hMap = new Bucket[buckets];
+        for(int i = 0; i < buckets; i++){
+            hMap[i] = new Bucket();
+        }
     }
-  }
-
-  /** value will always be non-negative. */
-  public void put(int key, int value) {
-    int hash_key = key % this.key_space;
-    this.hash_table.get(hash_key).update(key, value);
-  }
-
-  /**
-   * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping
-   * for the key
-   */
-  public int get(int key) {
-    int hash_key = key % this.key_space;
-    return this.hash_table.get(hash_key).get(key);
-  }
-
-  /** Removes the mapping of the specified value key if this map contains a mapping for the key */
-  public void remove(int key) {
-    int hash_key = key % this.key_space;
-    this.hash_table.get(hash_key).remove(key);
-  }
+    protected int _hash(int key){
+        return key % buckets;
+    }
+    public void put(int key, int value) {
+        hMap[_hash(key)].set(key, value);
+    }
+    
+    public int get(int key) {
+        return hMap[_hash(key)].getVal(key);
+    }
+    
+    public void remove(int key) {
+        hMap[_hash(key)].remove(key);
+    }
+}
+class Pair {
+    private int key;
+    private int val;
+    public Pair(int key, int val){
+        setKey(key);
+        setVal(val);
+    }
+    public void setKey(int key){
+        this.key = key;
+    }
+    public void setVal(int val){
+        this.val = val;
+    }
+    public int getKey(){ 
+        return this.key;
+    }
+    public int getVal(){
+        return this.val;
+    }
+    @Override
+    public boolean equals(Object obj){
+        Pair other = (Pair)obj;
+        return this.key == other.key;
+    }
+}
+class Bucket {    
+    private List<Pair> myList;
+    public Bucket(){
+        myList = new ArrayList<Pair>();
+    }
+    public int getVal(int key){
+        for(int i = 0; i < myList.size(); i++){
+            Pair p = myList.get(i);
+            if(p.getKey() == key){
+                return p.getVal();
+            }
+        }
+        return -1;
+    }
+    public void set(int key, int val){
+        Pair p = new Pair(key, val);
+        int loc = findLoc(key);
+        if(loc != -1){
+            this.myList.get(loc).setVal(val);
+        }else {
+            this.myList.add(p);
+        }        
+    }
+    public void remove(int key){
+        for(int i = 0; i < myList.size(); i++){
+            Pair p = myList.get(i);
+            if(p.getKey() == key){
+                myList.remove(i);
+                break;
+            }
+        }
+    }
+    private int findLoc(int key){
+        for(int i = 0; i < myList.size(); i++){
+            Pair p = myList.get(i);
+            if(p.getKey() == key){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
 
 /**
- * Your MyHashMap object will be instantiated and called as such: MyHashMap obj = new MyHashMap();
- * obj.put(key,value); int param_2 = obj.get(key); obj.remove(key);
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
  */
