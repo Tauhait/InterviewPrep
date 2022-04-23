b4d3d6f83620b4b37efdbcc0acc1f933cc2079a2
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-04-23 20:17:52 +0530

    Time: 2 ms (84.23%), Space: 76.4 MB (70.52%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,35 +1,13 @@
-class Solution {
-/** Kadane Algo
-// Given an input array of numbers "nums",
-1. best = negative infinity
-2. current = 0
-3. for num in nums:
-    3.1. current = Max(current + num, num)
-    3.2. best = Max(best, current)
-
-4. return best
-*/
-    
-    public int maxProfit(int[] prices) {
-        if(prices.length < 2){
-            return 0;
+public class Solution {
+    public int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)//buy if least price encountered till now 
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)//sell if maxprofit till now
+                maxprofit = prices[i] - minprice;
         }
-        int best = Integer.MIN_VALUE;
-        if(prices.length == 2){
-            best = prices[1] - prices[0];
-            return best < 0 ? 0 : best;
-        }
-        
-        int[] profit = new int[prices.length - 1];
-        for(int i = 1; i < prices.length; i++){
-            profit[i - 1] = prices[i] - prices[i - 1];
-        }
-        
-        int curr = 0;
-        for(int p : profit){
-            curr = Math.max(curr + p, p);
-            best = Math.max(best, curr);
-        }
-        return best < 0 ? 0 : best;
+        return maxprofit;
     }
 }
