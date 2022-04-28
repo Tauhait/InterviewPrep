abaaaf24829f213e05a1265c27d0b8c45632f2a0
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-04-29 01:31:26 +0530

    Time: 105 ms (35.88%), Space: 43.2 MB (78.43%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,59 +1,87 @@
 class Solution {
     public int minimumEffortPath(int[][] heights) {
-        int[][] directions = {{0,1}, {1,0}, {-1,0}, {0,-1}};
-        int rows = heights.length;
-        int cols = heights[0].length;
-        
-        boolean[][] visited = new boolean[rows][cols];
-        PriorityQueue<Peak> pQueue = new PriorityQueue<Peak>((a, b) -> (a.effort - b.effort));
-        int[][] hikes = new int[rows][cols];
-        
-        for(int row = 0; row < rows; row++){
-            for(int col = 0; col < cols; col++){
-                hikes[row][col] = Integer.MAX_VALUE;
+        int row = heights.length;
+        int col = heights[0].length;
+        if (row == 1 && col == 1) return 0;
+        UnionFind unionFind = new UnionFind(heights);
+        List<Edge> edgeList = unionFind.edgeList;
+        Collections.sort(edgeList, (e1, e2) -> e1.difference - e2.difference);
+
+        for (int i = 0; i < edgeList.size(); i++) {
+            int x = edgeList.get(i).x;
+            int y = edgeList.get(i).y;
+            unionFind.union(x, y);
+            if (unionFind.find(0) == unionFind.find(row * col - 1)){
+                return edgeList.get(i).difference;
             }
         }
-        
-        hikes[0][0] = 0;
-        visited[0][0] = true;
-        pQueue.add(new Peak(0, 0, hikes[0][0]));
-        
-        while(!pQueue.isEmpty()){
-            Peak thisPeak = pQueue.poll();
-            if(thisPeak.x == rows - 1 && thisPeak.y == cols - 1){
-                return thisPeak.effort;
-            }
-            visited[thisPeak.x][thisPeak.y] = true;
-            for(int[] dir : directions){
-                int newX = thisPeak.x + dir[0];
-                int newY = thisPeak.y + dir[1];
-                if(isValid(newX, newY, rows, cols) && !visited[newX][newY]){
-                    //calc hike to the adjacent peak
-                    int hike = Math.abs(heights[thisPeak.x][thisPeak.y] - heights[newX][newY]);
-                    //calc the route's bottleneck to newX, newY Peak: problem specific req
-                    int maxHike = Math.max(hikes[thisPeak.x][thisPeak.y], hike);
-                    //update hike needed
-                    hikes[newX][newY] = Math.min(hikes[newX][newY], maxHike);
-                    //add new peak into queue
-                    pQueue.add(new Peak(newX, newY, hikes[newX][newY]));         
-                }
-            }
-        }
-        return hikes[rows - 1][cols - 1];
-    }
-    
-    public boolean isValid(int x, int y, int limitX, int limitY){
-        return x >= 0 && x < limitX && y >= 0 && y < limitY;
+        return -1;
     }
 }
-class Peak {
+
+class UnionFind {
+    int[] parent;
+    int[] rank;
+    List<Edge> edgeList;
+
+    public UnionFind(int[][] heights) {
+        int row = heights.length;
+        int col = heights[0].length;
+        parent = new int[row * col];
+        edgeList = new ArrayList<>();
+        rank = new int[row * col];
+        for (int currentRow = 0; currentRow < row; currentRow++) {
+            for (int currentCol = 0; currentCol < col; currentCol++) {
+                if (currentRow > 0) {
+                    edgeList.add(new Edge(currentRow * col + currentCol,
+                            (currentRow - 1) * col + currentCol,
+                            Math.abs(heights[currentRow][currentCol] - heights[currentRow - 1][currentCol]))
+                    );
+                }
+                if (currentCol > 0) {
+                    edgeList.add(new Edge(currentRow * col + currentCol,
+                            currentRow * col + currentCol - 1,
+                            Math.abs(heights[currentRow][currentCol] - heights[currentRow][currentCol - 1]))
+                    );
+                }
+                parent[currentRow * col + currentCol] = currentRow * col + currentCol;
+            }
+        }
+    }
+
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    void union(int x, int y) {
+        int parentX = find(x);
+        int parentY = find(y);
+        if (parentX != parentY) {
+            if (rank[parentX] > rank[parentY]) {
+                parent[parentY] = parentX;
+            }
+            else if (rank[parentX] < rank[parentY]) {
+                parent[parentX] = parentY;
+            }
+            else {
+                parent[parentY] = parentX;
+                rank[parentX] += 1;
+            }
+        }
+    }
+}
+
+class Edge {
     int x;
     int y;
-    int effort;
-    
-    public Peak(int x, int y, int effort){
+    int difference;
+
+    Edge(int x, int y, int difference) {
         this.x = x;
         this.y = y;
-        this.effort = effort;
+        this.difference = difference;
     }
 }
