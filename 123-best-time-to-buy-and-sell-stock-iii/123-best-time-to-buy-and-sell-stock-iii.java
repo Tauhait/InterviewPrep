e87cc6148b3462289d10192fc48d4c9234ef0f07
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-05-01 23:57:14 +0530

    Time: 5 ms (67.48%), Space: 78.9 MB (50.54%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,27 +1,19 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int dp[][][] = new int[n + 1][2 + 1][2];
-        
-        for (int i = n - 1; i >= 0; i--) {
-            for (int transactionsRemaining = 1; transactionsRemaining <= 2; transactionsRemaining++) {
-                for (int holding = 0; holding < 2; holding++) {
-                    int doNothing = dp[i + 1][transactionsRemaining][holding];
-                    int doSomething;
-                    if (holding == 1) {
-                        // Sell stock
-                        doSomething = prices[i] + dp[i + 1][transactionsRemaining - 1][0];
-                    } else {
-                        // Buy stock
-                        doSomething = -prices[i] + dp[i + 1][transactionsRemaining][1];
-                    }
-                    
-                    // Recurrence relation
-                    dp[i][transactionsRemaining][holding] = Math.max(doNothing, doSomething);
-                }
-            }
+        int t1Cost = Integer.MAX_VALUE, 
+            t2Cost = Integer.MAX_VALUE;
+        int t1Profit = 0,
+            t2Profit = 0;
+
+        for (int price : prices) {
+            // the maximum profit if only one transaction is allowed
+            t1Cost = Math.min(t1Cost, price);
+            t1Profit = Math.max(t1Profit, price - t1Cost);
+            // reinvest the gained profit in the second transaction
+            t2Cost = Math.min(t2Cost, price - t1Profit);
+            t2Profit = Math.max(t2Profit, price - t2Cost);
         }
-        
-        return dp[0][2][0];
+
+        return t2Profit;
     }
 }
