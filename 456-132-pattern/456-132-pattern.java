3813101e23dd1c7947642b72e7bda49535c8bc1a
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-05-07 13:39:40 +0530

    Time: 48 ms (32.63%), Space: 66.8 MB (46.95%) - LeetHub

--- 456-132-pattern/456-132-pattern.java
+++ 456-132-pattern/456-132-pattern.java
@@ -1,18 +1,24 @@
 class Solution {
     public boolean find132pattern(int[] nums) {
+        if (nums.length < 3){
+            return false;
+        }
         int[] min = new int[nums.length];
         min[0] = nums[0];
         for(int i = 1; i < nums.length; i++){
             min[i] = Math.min(min[i - 1], nums[i]);
         }
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>();//mono stack to store sorted seqeunce for every j
         for(int j = nums.length - 1; j >= 0 ; j--){
+            //we start from backwards to ensure the min[j] is the global minimum
+            //implementation os mono stack 
+            //stack.peek() refers to the kth index value
             while(!stack.isEmpty() && stack.peek() < nums[j]){// incr stack (top to bottom)
                 //top element of stack is least
                 if(stack.peek() > min[j]){
                     return true;
                 }
-                stack.pop();
+                stack.pop();//ensures nums[j] is the smallest value at stack top
             }
             stack.push(nums[j]);
         }
