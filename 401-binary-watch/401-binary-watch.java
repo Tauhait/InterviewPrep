600a6412e2e0a0eb1ee4f4d5376e68e2f94bf85f
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-05-09 11:12:41 +0530

    Time: 24 ms (15.22%), Space: 42.9 MB (41.97%) - LeetHub

--- 401-binary-watch/401-binary-watch.java
+++ 401-binary-watch/401-binary-watch.java
@@ -1,15 +1,49 @@
 class Solution {
+    private int[] hourDict = new int[]{ 8, 4, 2, 1};
+    private int[] minuteDict = new int[]{ 32, 16, 8, 4, 2, 1};
+    
     public List<String> readBinaryWatch(int turnedOn) {
-        List<String> times = new ArrayList<>();
-        int shift = 1 << 6;
-        for (int h = 0; h < 12; h++){ 
-            for (int m = 0; m < 60; m++){ 
-                if (Integer.bitCount(h * shift + m) == turnedOn){
-                    times.add(String.format("%d:%02d", h, m));
-                }                    
-            }                
+        List<String> result = new ArrayList<>();
+        if(turnedOn == 0){
+            result.add("0:00");
+            return result;
         }
-            
-        return times;  
+
+        for(int i = 0; i <= turnedOn; i++){
+            List<Integer> hours = getHours(i);
+            List<Integer> minutes = getMinutes(turnedOn - i);
+
+            for(Integer hour : hours){
+                for(Integer minute : minutes){
+                    //String temp = String.format("%d:%02d", hour, minute);
+                    result.add(String.format("%d:%02d", hour, minute));
+                }
+            }
+        }
+        return result;
+    }
+
+    private List<Integer> getHours(int turnedOn){
+        List<Integer> list = new ArrayList<>();
+        helper(hourDict, 0, 0, 0, turnedOn, list, 12);
+        return list;
+    }
+    
+    private List<Integer> getMinutes(int turnedOn){
+        List<Integer> list = new ArrayList<>();
+        helper(minuteDict, 0, 0, 0, turnedOn, list, 60);
+        return list;
+    }
+
+    private void helper(int[] array, int curSum, int index, int level, int turnedOn, List<Integer> list, int max){
+        if(level == turnedOn && curSum < max){
+            list.add(curSum);
+            return;
+        }
+        for(int i = index; i < array.length; i++){
+            curSum += array[i];
+            helper(array, curSum, i + 1, level + 1, turnedOn, list, max);
+            curSum -= array[i];
+        }
     }
 }
