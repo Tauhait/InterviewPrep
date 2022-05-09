3720a27749a42b22e495c260fc0c2278ad536b7f
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-05-09 19:45:15 +0530

    Time: 1 ms (98.28%), Space: 43.1 MB (18.63%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -2,19 +2,17 @@ class Solution {
     private Integer[][] memo;
     private String s1, s2, s3;
     public int dp(int i, int j, int k) {
-        if (i == s1.length()) {
-            return s2.substring(j).equals(s3.substring(k)) ? 1 : 0;
+        if (i < 0) {
+            return s2.substring(0, j + 1).equals(s3.substring(0, k + 1)) ? 1 : 0;
         }
-        if (j == s2.length()) {
-            return s1.substring(i).equals(s3.substring(k)) ? 1 : 0;
+        if (j < 0) {
+            return s1.substring(0, i + 1).equals(s3.substring(0, k + 1)) ? 1 : 0;
         }
         if (memo[i][j] != null) {
             return memo[i][j];
         }
-        // boolean ans = false;
-        if (s3.charAt(k) == s1.charAt(i) && dp(i + 1, j, k + 1) == 1 || 
-            s3.charAt(k) == s2.charAt(j) && dp(i, j + 1, k + 1) == 1   ) {
-            // ans = true;
+        if (s3.charAt(k) == s1.charAt(i) && dp(i - 1, j, k - 1) == 1 || 
+            s3.charAt(k) == s2.charAt(j) && dp(i, j - 1, k - 1) == 1   ) {
             memo[i][j] = 1;
         }else {
             memo[i][j] = 0;
@@ -30,12 +28,7 @@ class Solution {
         this.s1 = s1;
         this.s2 = s2;
         this.s3 = s3;
-        // for (int i = 0; i < s1.length(); i++) {
-        //     for (int j = 0; j < s2.length(); j++) {
-        //         memo[i][j] = -1;
-        //     }
-        // }
-        // return dp(s1, 0, s2, 0, s3, 0);
-        return dp(0, 0, 0) == 1 ? true : false;
+
+        return dp(s1.length() - 1, s2.length() - 1, s3.length() - 1) == 1 ? true : false;
     }
 }
