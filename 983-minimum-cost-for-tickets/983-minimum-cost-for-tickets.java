b3dd857c1849ad676ef22c5517a5433629f02929
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-05-09 11:29:28 +0530

    Time: 8 ms (8.25%), Space: 42.5 MB (26.78%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
@@ -10,11 +10,11 @@ class Solution {
         for (int d: days) {
             dayset.add(d);
         }
-        return dp(1);
+        return dp(365);
     }
 
     private int dp(int i) {
-        if (i > 365){ 
+        if (i < 1){ 
             return 0;
         }
         if (memo[i] != null){ 
@@ -23,10 +23,10 @@ class Solution {
         if (dayset.contains(i)) {
             //if I'm travelling on ith day then find the minimum cost using 1-, 7-, and 
             //30- day pass variants
-            memo[i] = Math.min(dp(i + 1) + costs[0],
-                               Math.min(dp(i + 7) + costs[1], dp(i + 30) + costs[2]));
+            memo[i] = Math.min(dp(i - 1) + costs[0],
+                               Math.min(dp(i - 7) + costs[1], dp(i - 30) + costs[2]));
         } else {//not trvalling on ith day, skip
-            memo[i] = dp(i+1);
+            memo[i] = dp(i - 1);
         }
         return memo[i];
     }
