158b1a44ca98b72e639562e87a4039a19731a44b
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-05-12 11:53:23 +0530

    Time: 14 ms (69.13%), Space: 52.3 MB (5.24%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,33 +1,31 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // O(1) time
-        if (k == nums.length) {
+        if(nums.length == k){
             return nums;
         }
+        Map<Integer, Integer> freqMap = new HashMap();
+        for(int n : nums){
+            freqMap.put(n, freqMap.getOrDefault(n, 0) + 1);
+        }
+        List<Integer>[] buckets = new ArrayList[nums.length + 1];
+        for(int i = 1; i <= nums.length; i++){
+            buckets[i] = new ArrayList<Integer>();
+        }
+        for(int key : freqMap.keySet()){
+            buckets[freqMap.get(key)].add(key);
+        }
         
-        // 1. build hash map : character and how often it appears
-        // O(N) time
-        Map<Integer, Integer> count = new HashMap();
-        for (int n: nums) {
-          count.put(n, count.getOrDefault(n, 0) + 1);
-        }
-
-        // init heap 'the less frequent element first'
-        Queue<Integer> heap = new PriorityQueue<>(
-            (n1, n2) -> count.get(n1) - count.get(n2));
-
-        // 2. keep k top frequent elements in the heap
-        // O(N log k) < O(N log N) time
-        for (int n: count.keySet()) {
-          heap.add(n);
-          if (heap.size() > k) heap.poll();    
-        }
-
-        // 3. build an output array
-        // O(k log k) time
         int[] top = new int[k];
-        for(int i = k - 1; i >= 0; --i) {
-            top[i] = heap.poll();
+        int j = nums.length;
+        for(int i = k - 1; i >= 0; ){
+            if(buckets[j].size() == 0){
+                j--;
+                continue;
+            }
+            List<Integer> bucketList = buckets[j--];
+            for(int b : bucketList){
+                top[i--] = b;
+            }
         }
         return top;
     }
