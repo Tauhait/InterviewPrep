59777b73473869d295729e023c057308aecb08e4
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-05-17 19:09:10 +0530

    Time: 68 ms (26.39%), Space: 117.4 MB (41.92%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,14 +1,8 @@
 class Solution {
-    public int[][] kClosest(int[][] points, int k) {
-        
-        // Map<String, Integer> pointDistMap = new HashMap();
-        
+    public int[][] kClosest(int[][] points, int k) {        
         PriorityQueue<int[]> maxHeap = new PriorityQueue<int[]>((a, b) -> (b[2] - a[2]));
         for(int[] point : points){
-            // String p = point[0] + "," + point[1];
             int dist = (int)(Math.pow(point[0],2) + Math.pow(point[1],2));
-            
-            // pointDistMap.put(p, dist);            
             maxHeap.add(new int[]{point[0], point[1], dist});
             if(maxHeap.size() > k){
                 maxHeap.poll();
