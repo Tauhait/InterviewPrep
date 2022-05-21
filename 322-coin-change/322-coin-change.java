8c45ac2d6a20764821f10de23c43e3bc971f9bd9
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-05-21 10:28:11 +0530

    Time: 475 ms (5.00%), Space: 114.6 MB (6.15%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -3,20 +3,23 @@ class Solution {
     private int[] coins;
     
     private int dp(int money){
-        if(money < 0){
-            return -1;
-        }
+        // if(money < 0){
+        //     return -1;
+        // }
         if(money == 0){ 
             return 0;
         }
         int minCoins = Integer.MAX_VALUE;
         if(!memo.containsKey(money)){            
             for(int coin : coins){
+                if(money - coin < 0){
+                    continue;
+                }
                 int res = dp(money - coin);
                 if(res >= 0 && res < minCoins){
                     minCoins = 1 + res;
-                }                
-            }        
+                }    
+            }    
             memo.put(money, minCoins == Integer.MAX_VALUE ? -1 : minCoins);               
         }
         return memo.get(money);
