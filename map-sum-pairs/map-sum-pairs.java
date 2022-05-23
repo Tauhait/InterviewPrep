77b8826a9980933ea6dc2440f0277ca73a80bc4f
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-05-24 00:49:42 +0530

    Time: 28 ms, Memory: 42.3 MB - LeetHub

---
+++ map-sum-pairs/map-sum-pairs.java
@@ -0,0 +1,38 @@
+class TrieNode {
+    Map<Character, TrieNode> children = new HashMap();
+    int score;
+}
+class MapSum {
+    HashMap<String, Integer> map;
+    TrieNode root;
+    public MapSum() {
+        map = new HashMap();
+        root = new TrieNode();
+    }
+    public void insert(String key, int val) {
+        int delta = val - map.getOrDefault(key, 0);
+        map.put(key, val);
+        TrieNode cur = root;
+        cur.score += delta;
+        for (char c: key.toCharArray()) {
+            cur.children.putIfAbsent(c, new TrieNode());
+            cur = cur.children.get(c);
+            cur.score += delta;
+        }
+    }
+    public int sum(String prefix) {
+        TrieNode cur = root;
+        for (char c: prefix.toCharArray()) {
+            cur = cur.children.get(c);
+            if (cur == null) return 0;
+        }
+        return cur.score;
+    }
+}
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum obj = new MapSum();
+ * obj.insert(key,val);
+ * int param_2 = obj.sum(prefix);
+ */
