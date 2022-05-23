39f2deb3ac6a3129e89fc29d0bb9d7e45073571f
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-05-23 12:01:26 +0530

    Time: 5 ms (75.15%), Space: 41.9 MB (69.69%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,36 +1,19 @@
 class Solution {
-    private Boolean[][] memo;
-    private String s;
-    
-    private Boolean dp(int i, int j){
-        if(memo[i][j] != null){
-            return memo[i][j];
+    public int countSubstrings(String str) {
+        if(str == null || str.length() < 1) return 0;
+        int globalCount = 0;
+        for(int i = 0;i < str.length(); i++){
+            globalCount += countPalindromes(str, i, i) + countPalindromes(str, i, i + 1); 
         }
-        
-        if(i >= j){
-            return true;
-        }else if(s.charAt(i) != s.charAt(j)){
-            return false;
-        }else {
-            memo[i][j] = dp(i + 1, j - 1);
+        return globalCount;
+    }    
+    private int countPalindromes(String str, int start, int end){
+        int localCount = 0;
+        while(start >= 0 && end < str.length() && str.charAt(start) == str.charAt(end)){
+            start--;
+            end++;
+            localCount++;
         }
-        return memo[i][j];        
-    }
-    public int countSubstrings(String s) {
-        int slen = s.length();
-        if(slen == 0){
-            return 0;
-        }
-        
-        memo = new Boolean[slen][slen];
-        this.s = s;
-        int ans = 0;
-        
-        for(int i = 0; i < slen; i++){
-            for(int gap = i; gap < slen; gap++){
-                ans += (dp(i, gap) ? 1 : 0);
-            }
-        }        
-        return ans;
+        return localCount;
     }
 }
