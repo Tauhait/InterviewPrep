8cc6a0bc9b305d2ac40b5ba5f8b1457170ee3695
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-05-28 11:07:20 +0530

    Time: 219 ms (44.75%), Space: 140.5 MB (13.65%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.java
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.java
@@ -1,59 +1,57 @@
+
 class Solution {
-    private class TrieNode {
-      HashMap<Character, TrieNode> children;
+  private class TrieNode {
+      TrieNode[] children;
       public TrieNode() {
-          children = new HashMap<Character, TrieNode>();
+        children = new TrieNode[2];
+      }
+  }
+
+  public int findMaximumXOR(int[] nums) {
+    if (null == nums || 0 == nums.length)
+      return 0;
+    
+    // get the longest binary representation.
+    int maxLen = 0;
+    for (int x : nums)
+      maxLen = Math.max(maxLen, Integer.toBinaryString(x).length());
+    
+    // construct binary representation for each number.
+    String[] strs = new String[nums.length];
+    int padding = (1 << maxLen);
+    for (int i = 0; i < nums.length; ++i)
+      strs[i] = (Integer.toBinaryString(padding | nums[i]).substring(1));
+    
+    // construct trie first, and then get the max from the trie.
+    TrieNode root = new TrieNode();
+    for (String x : strs) {
+      TrieNode curr = root;
+      for (char c : x.toCharArray()) {
+        int bit = c - '0';
+        if (null == curr.children[bit])
+          curr.children[bit] = new TrieNode();
+        curr = curr.children[bit];
       }
     }
-    public int findMaximumXOR(int[] nums) {
-        // Compute length L of max number in a binary representation
-        int maxNum = nums[0];
-        for(int num : nums) {
-            maxNum = Math.max(maxNum, num);
+    
+    int result = 0;
+    for (String x : strs) {
+      TrieNode curr = root;
+      int currRes = 0;
+      for (char c : x.toCharArray()) {
+        int bit = c - '0';
+        if (null != curr.children[1 - bit]) {
+          currRes = (currRes << 1) | 1;
+          curr = curr.children[1 - bit];
+        } else {
+          currRes <<= 1;
+          curr = curr.children[bit];
         }
-        int L = (Integer.toBinaryString(maxNum)).length();
-
-        // zero left-padding to ensure L bits for each number
-        int n = nums.length;
-        int bitmask = 1 << L;
-        String [] strNums = new String[n];
-        
-        for(int i = 0; i < n; ++i) {
-        //convert into string and insert as string without the leftmost bit
-          strNums[i] = Integer.toBinaryString(bitmask | nums[i]).substring(1);
-        }
-
-        TrieNode trie = new TrieNode();
-        
-        int maxXor = 0;
-        
-        for (String num : strNums) {
-          TrieNode node = trie;//node use to insert into Trie
-          TrieNode xorNode = trie;//node used to perform XORing curr num
-          int currXor = 0;
-          for (Character bit : num.toCharArray()) {
-            // insert new number in trie  
-            if (node.children.containsKey(bit)) {
-              node = node.children.get(bit);
-            } else {
-              TrieNode newNode = new TrieNode();
-              node.children.put(bit, newNode);
-              node = newNode;
-            }
-
-            // compute max xor of that new number 
-            // with all previously inserted
-            Character toggledBit = bit == '1' ? '0' : '1';
-            if (xorNode.children.containsKey(toggledBit)) {
-              currXor = (currXor << 1) | 1;
-              xorNode = xorNode.children.get(toggledBit);
-            } else {
-              currXor = currXor << 1;
-              xorNode = xorNode.children.get(bit);
-            }
-          }
-          maxXor = Math.max(maxXor, currXor);
-        }
-        return maxXor;
-     }
+      }
+      
+      result = Math.max(result, currRes);
+    }
+    
+    return result;
+  }
 }
