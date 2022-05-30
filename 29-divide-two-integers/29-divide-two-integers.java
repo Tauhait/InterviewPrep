c4659bc5ff7ac29e591297f6f17baca2fecd0034
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-05-30 22:23:44 +0530

    Time: 4 ms (14.19%), Space: 41.4 MB (47.43%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.java
+++ 29-divide-two-integers/29-divide-two-integers.java
@@ -1,5 +1,5 @@
 class Solution {
-    private static int HALF_INT_MIN = -1073741824;
+    private static int HALF_INT_MIN = -1073741824;// -2**30;
 
     public int divide(int dividend, int divisor) {
 
@@ -20,25 +20,34 @@ class Solution {
             divisor = -divisor;
         }
 
-        int quotient = 0;
-        /* Once the divisor is bigger than the current dividend,
-         * we can't fit any more copies of the divisor into it. */
+        ArrayList<Integer> doubles = new ArrayList<>();
+        ArrayList<Integer> powersOfTwo = new ArrayList<>();
+
+        /* Nothing too exciting here, we're just making a list of doubles of 1 and
+         * the divisor. This is pretty much the same as Approach 2, except we're
+         * actually storing the values this time. */
+        int powerOfTwo = -1;
         while (divisor >= dividend) {
-            /* We know it'll fit at least once as divivend >= divisor.
-             * Note: We use a negative powerOfTwo as it's possible we might have
-             * the case divide(INT_MIN, -1). */
-            int powerOfTwo = -1;
-            int value = divisor;
-            /* Check if double the current value is too big. If not, continue doubling.
-             * If it is too big, stop doubling and continue with the next step */
-            while (value >= HALF_INT_MIN && value + value >= dividend) {
-                value += value;
-                powerOfTwo += powerOfTwo;
+            doubles.add(divisor);
+            powersOfTwo.add(powerOfTwo);
+            // Prevent needless overflows from occurring...
+            if (divisor < HALF_INT_MIN) {
+                break;
             }
-            // We have been able to subtract divisor another powerOfTwo times.
-            quotient += powerOfTwo;
-            // Remove value so far so that we can continue the process with remainder.
-            dividend -= value;
+            divisor += divisor;
+            powerOfTwo += powerOfTwo;
+        }
+
+        int quotient = 0;
+        /* Go from largest double to smallest, checking if the current double fits.
+         * into the remainder of the dividend. */
+        for (int i = doubles.size() - 1; i >= 0; i--) {
+            if (doubles.get(i) >= dividend) {
+                // If it does fit, add the current powerOfTwo to the quotient.
+                quotient += powersOfTwo.get(i);
+                // Update dividend to take into account the bit we've now removed.
+                dividend -= doubles.get(i);
+            }
         }
 
         /* If there was originally one negative sign, then
