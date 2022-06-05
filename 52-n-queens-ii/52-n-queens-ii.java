c934086bcdf0e53e28367d4139d3937ef32be713
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-06-05 13:23:57 +0530

    Time: 7 ms (32.17%), Space: 41.4 MB (36.40%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.java
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -1,62 +1,29 @@
-/*
-def backtrack_nqueen(row = 0, count = 0):
-    for col in range(n):
-        # iterate through columns at the curent row.
-        if is_not_under_attack(row, col):
-            # explore this partial candidate solution, and mark the attacking zone
-            place_queen(row, col)
-            if row + 1 == n:
-                # we reach the bottom, i.e. we find a solution!
-                count += 1
-            else:
-                # we move on to the next row
-                count = backtrack_nqueen(row + 1, count)
-            # backtrack, i.e. remove the queen and remove the attacking zone.
-            remove_queen(row, col)
-    return count
-*/
-
 class Solution {
-    private int count = 0;
-    private LinkedList<Integer> rows = new LinkedList<Integer>();
-    private LinkedList<Integer> cols = new LinkedList<Integer>();
     public int totalNQueens(int n) {
-        backtrackNQueens(n, 0);
-        return count;
+        return backtrack(n, 0,  new HashSet<Integer>(), 
+                                new HashSet<Integer>(), 
+                                new HashSet<Integer>());
     }
-    private void backtrackNQueens(int n, int row){
-        for(int col = 0; col < n; col++){
-            if(isNotUnderAttack(row, col)){
-                placeQueen(row, col);
-                if(row + 1 == n){
-                    count++;
-                }else {
-                    backtrackNQueens(n, row + 1);
-                }
-                removeQueen(row, col);
-            }
-        }        
-    }
-    private void removeQueen(int row, int col){
-        rows.pollLast();
-        cols.pollLast();
-    }
-    private void placeQueen(int row, int col){
-        rows.add(row);
-        cols.add(col);
-    }
-    private boolean isNotUnderAttack(int row, int col){
-        if(rows.contains(row) || cols.contains(col)){
-            return false;
+    private int backtrack(int size, int row, Set<Integer> cols, Set<Integer> diags, Set<Integer> antiDiags){
+        if(row == size) return 1;
+        
+        int solutions = 0;
+        for(int col = 0; col < size; col++){
+            int diag = row - col;
+            int antiDiag = row + col;
+            
+            if(cols.contains(col) || diags.contains(diag) || antiDiags.contains(antiDiag)) continue;
+            
+            cols.add(col);
+            diags.add(diag);
+            antiDiags.add(antiDiag);
+            
+            solutions += backtrack(size, row + 1, cols, diags, antiDiags);
+            
+            cols.remove(col);
+            diags.remove(diag);
+            antiDiags.remove(antiDiag);            
         }
-        int size = rows.size();
-        for(int index = 0; index < size; index++){
-            int r = rows.get(index);
-            int c = cols.get(index);
-            if(Math.abs(row - r) == Math.abs(col - c)){
-                return false;
-            }
-        }
-        return true;               
+        return solutions;
     }
 }
