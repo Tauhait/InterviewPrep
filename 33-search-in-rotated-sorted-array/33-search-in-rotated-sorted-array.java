3eafa3affdf66987da868d57ea640e7c46316d1b
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-06-06 15:22:35 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (95.01%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,52 +1,18 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums == null || nums.length == 0)
-            return -1;
-        
-        if(nums.length == 1)
-            return (nums[0] == target ? 0 : -1);
-        if(nums.length == 2)
-            return (nums[0] == target ? 0 : (nums[1] == target ? 1 : -1));        
-        
-        int len = nums.length;       
-        int low = 0, high = len - 1, mid = low + (high - low)/2;          
-        return searchRotArray(low, high, nums, target);           
-    }
-    public int searchRotArray(int low, int high, int[] nums, int target){
-        while(low <= high){            
-            int mid = low + (high - low)/2;  
-            // System.out.println("Mid: " +mid+", Low: "+low+", High: "+high+" ||| Value at mid: "+nums[mid]);
+        int low = 0, high = nums.length - 1;
+        while(low <= high){
+            int mid = (low + high) >>> 1;
             
-            if(nums[low] == target)
-                return low;
-            if(nums[high] == target)
-                return high;
-            if(nums[mid] == target)
-                return mid;
-            
-            int f = nums[low];
-            int l = nums[high];
-            if(l < f){
-                if(target > nums[mid] && target < l){
-                    low = mid + 1;
-                } else if(target < nums[mid] && target > f){
-                    high = mid - 1;
-                } else {                                            
-                    int retVal = searchRotArray(low, mid - 1, nums, target);
-                    if(retVal == -1){
-                        retVal = searchRotArray(mid + 1, high, nums, target);
-                    } 
-                    return retVal;                   
-                }
-            } else {
-                if(target < nums[mid]){
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] >= nums[low]){//non-rotated side
+                if(target >= nums[low] && target < nums[mid]) high = mid - 1;
+                else low = mid + 1;
+            }else {//rotated side
+                if(target > nums[mid] && target <= nums[high]) low = mid + 1;
+                else high = mid - 1;
             }
         }
-        
         return -1;
     }
 }
