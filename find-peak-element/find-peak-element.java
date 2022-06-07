2c1740bc10d38413935714fd87d7d519accc9edd
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-06-07 19:04:56 +0530

    Time: 0 ms, Memory: 42.7 MB - LeetHub

---
+++ find-peak-element/find-peak-element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int len = nums.length;
+        int low = 0, high = len - 1;
+        while(low < high){
+            int mid = low + (high - low)/2;
+            
+            //intuition: go towards the peak of the current asc/desc seq by comparing (mid) & (mid + 1)
+            //if desc then go towards left to find peak
+            //if asc go towards right to find peak
+            //also note contrainsts to be assumed nums[-1] = nums[n] = -∞.
+            
+            if(nums[mid] > nums[mid + 1]){//peak is in left side
+                high = mid;
+            }else {//peak is in right side
+                low = mid + 1;
+            }            
+        }
+        return low;
+    }
+}
