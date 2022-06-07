d85d79422007f7a567967484b0dd43e6e7e776e3
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-06-07 11:11:22 +0530

    Time: 0 ms (100.00%), Space: 43 MB (28.54%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,5 +1,6 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        //Three pointers: m, n and finished
         int tail1 = m - 1, tail2 = n - 1, finished = m + n - 1;
         while (tail1 >= 0 && tail2 >= 0) {
             nums1[finished--] = (nums1[tail1] > nums2[tail2]) ? 
