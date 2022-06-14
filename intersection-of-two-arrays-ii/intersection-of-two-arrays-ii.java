de5ed27a6d2f0a6301c6217ebf51002d52f6d7c0
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-06-14 08:17:57 +0530

    Time: 3 ms, Memory: 44.8 MB - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,42 +1,19 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        if(nums1 == null || nums2 == null){
-            return new int[]{};
+        if (nums1.length > nums2.length) return intersect(nums2, nums1);
+    
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int n : nums1) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
         }
-        
-        Map<Integer, Integer> intersectMap;
-        List<Integer> ansList;
-        
-        intersectMap = getSmallArrMap(nums1);
-        ansList = intersectList(nums2, intersectMap);
-        return buildAns(ansList);
-    }
-    private int[] buildAns(List<Integer> ansList){
-        int len = ansList.size();
-        int[] ans = new int[len];
-        for(int idx = 0; idx < len; idx++){
-            ans[idx] = ansList.get(idx);
-        }
-        return ans;
-    }
-    private List<Integer> intersectList(int[] nums, Map<Integer, Integer> intersectMap){
-        List<Integer> ansList = new LinkedList<Integer>();
-        for(int i = 0; i < nums.length; i++){
-            if(intersectMap.containsKey(nums[i])){
-                ansList.add(nums[i]);
-                intersectMap.put(nums[i], intersectMap.get(nums[i]) - 1);
-                if(intersectMap.get(nums[i]) == 0){
-                     intersectMap.remove(nums[i]);
-                }
+        int k = 0;
+        for (int n : nums2) {
+            int cnt = map.getOrDefault(n, 0);
+            if (cnt > 0) {
+                nums1[k++] = n;//changing original array, ask if allowed
+                map.put(n, cnt - 1);
             }
         }
-        return ansList;
-    }
-    private Map<Integer, Integer> getSmallArrMap(int[] nums){
-        Map<Integer, Integer> intersectMap = new HashMap<Integer, Integer>();
-        for(int idx = 0; idx < nums.length; idx++){
-            intersectMap.put(nums[idx], intersectMap.getOrDefault(nums[idx], 0) + 1);
-        }
-        return intersectMap;
+        return Arrays.copyOfRange(nums1, 0, k);
     }
 }
