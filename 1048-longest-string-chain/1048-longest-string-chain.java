dae9be09bb64b302a7fea286127d566cecf2c929
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-06-16 00:11:02 +0530

    Time: 65 ms (49.82%), Space: 54.4 MB (51.22%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,68 +1,36 @@
-class Solution {
-    //valid ques to interviewer - is the letters in the word unique?
-    private Map<String, Integer> memo;
-    
-    private int recurse(String[] words, int i){//words can be made class variable, i is dp index
-        if(i < 0) return 1;//if no more words left then return 1 as last word was valid
-        if(memo.containsKey(words[i])) return memo.get(words[i]);
-        
-        String current = words[i];
-        int currLen = current.length();
-        List<Integer> candidates = new ArrayList<Integer>();//list of current predecessors
-        int j = i-1;//start from previous word to search predecessors
-        
-        for(; j >= 0; j--){
-            if(currLen - words[j].length() == 0) continue;//same length words skip
-            if(currLen - words[j].length() > 1) break;//diff between curr word and this word > 1
-            if(isPredecessor(words[j], current)){//diff = 1, so check if valid predecessor
-                candidates.add(j);
-            }
+class Solution { 
+    private int dfs(Set<String> words, Map<String, Integer> memo, String currentWord) {
+        if (memo.containsKey(currentWord)) {
+            return memo.get(currentWord);
         }
-        int count = 1;//considering the current word 
-        for(int pred : candidates){//traverse all list of predecessors
-            count = Math.max(count, 1 + recurse(words, pred));//recursively call candidates
-        }
-        memo.put(current, count);
-        return count;        
-    }
-    private boolean isPredecessor(String s1, String s2){//O(Max(n, m)); n = s1 len, m = s2 len
-        //both string differ by 1 in length, s1 < s2
-        char[] s1Char = s1.toCharArray();
-        char[] s2Char = s2.toCharArray();
+        // This stores the maximum length of word sequence possible with the 'currentWord' as the
+        int maxLength = 1;
+        StringBuilder sb = new StringBuilder(currentWord);
 
-        int s1Index = 0, s2Index = 0;
-        boolean oneChange = false;
-        
-        while(s1Index < s1.length() && s2Index < s2.length()){
-            if(s1Char[s1Index] == s2Char[s2Index]){
-                s1Index++;
-                s2Index++;
-            }else if(!oneChange){
-                s2Index++;
-                oneChange = true;
-            }else return false;
+        // creating all possible strings taking out one character at a time from the `currentWord`
+        for (int i = 0; i < currentWord.length(); i++) {
+            sb.deleteCharAt(i);
+            String newWord = sb.toString();
+            // If the new word formed is present in the list, we do a dfs search with this newWord.
+            if (words.contains(newWord)) {
+                int currentLength = 1 + dfs(words, memo, newWord);
+                maxLength = Math.max(maxLength, currentLength);
+            }
+            sb.insert(i, currentWord.charAt(i));
         }
-        return true;
-        
+        memo.put(currentWord, maxLength);
+
+        return maxLength;
     }
+
     public int longestStrChain(String[] words) {
-        if(words.length == 1) return 1;
-        memo = new HashMap<String, Integer>();
-        Arrays.sort(words, (a, b)->(a.length() - b.length()));//O(n log n)
-        int ans = 1;
-        for(int i = words.length-1; i >= 0; i--){
-            ans = Math.max(ans, recurse(words, i));
-            memo.put(words[i], ans);
+        Map<String, Integer> memo = new HashMap<>();
+        Set<String> wordsPresent = new HashSet<>();
+        Collections.addAll(wordsPresent, words);
+        int ans = 0;
+        for (String word : words) {
+            ans = Math.max(ans, dfs(wordsPresent, memo, word));
         }
         return ans;
     }
-    
 }
-/**
-1. Sort by length O(n log2 n)
-2. Recursively we need to call n - 1 length words and check if they can be candidates of the solution O(n)
-2.a Evaluate the n - 1 length word with the current word letter by letter and if there is only one letter mismatch then it is possible candidate O(m)
-3. Evaluate all candidates recursively and get the max chain value 
-
-O(n lg n + nm) = O(nm) = o(n^2)
-*/
