d52f1de995464add4baeea9e57f2cab45fe11e04
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-06-19 23:23:22 +0530

    Time: 68 ms (46.54%), Space: 77.5 MB (27.40%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,46 +1,45 @@
-class Solution {
-  public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-    TrieNode root = new TrieNode();
-    for (String prod : products) {
-      TrieNode curr = root;
-      for (char c : prod.toCharArray()) {
-        if (curr.children[c - 'a'] == null) {
-          curr.children[c - 'a'] = new TrieNode();
-        }
-        curr = curr.children[c - 'a'];
-        curr.matchingProducts.add(prod);
-      }
-    }
-    List<List<String>> result = new ArrayList<>();
-    TrieNode curr = root;
-    for (char c : searchWord.toCharArray()) {
-      if (curr == null) {
-        result.add(new ArrayList<>());
-      } else {
-        if (curr.children[c - 'a'] == null) {
-          curr = null;
-          result.add(new ArrayList<>());
-        } else {
-          curr = curr.children[c - 'a'];
-          PriorityQueue<String> currMatchingProductsQueue = new PriorityQueue<>(curr.matchingProducts);
-          List<String> currMatchingProducts = new ArrayList<>();
-          while (!currMatchingProductsQueue.isEmpty() && currMatchingProducts.size() < 3) {
-            currMatchingProducts.add(currMatchingProductsQueue.poll());
-          }
-          result.add(currMatchingProducts);
-        }
-      }
-    }
-    return result;
-  }
-  
-  private class TrieNode {
+class TrieNode {
     TrieNode[] children;
-    PriorityQueue<String> matchingProducts;
-    
+    PriorityQueue<String> pq;
+
     public TrieNode() {
-      this.children = new TrieNode[26];
-      this.matchingProducts = new PriorityQueue<>();
+        children = new TrieNode[26];
+        pq = new PriorityQueue<>((a,b) -> b.compareTo(a));
     }
-  }
+
+    public void addToPQ(String word) {
+        pq.add(word);
+        if (pq.size() > 3) pq.poll();
+    }
+
+    public List<String> getTopThree() {
+        List<String> topThree = new ArrayList<>();
+        while (!pq.isEmpty()) topThree.add(pq.poll());
+        Collections.reverse(topThree);
+        return topThree;
+    }
+}
+class Solution {
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        TrieNode root = new TrieNode();
+        for (String product: products) insert(root, product);
+
+        List<List<String>> results = new ArrayList<>();
+        for (char c: searchWord.toCharArray()) {
+            if ((root = root.children[c - 'a']) == null) break;
+            results.add(root.getTopThree());            
+        }
+
+        while (results.size() < searchWord.length()) 
+            results.add(new ArrayList<>());
+        return results;
+    }
+    private void insert(TrieNode root, String word) {
+        for (char c : word.toCharArray()) {
+            if (root.children[c - 'a'] == null) 
+                root.children[c - 'a'] = new TrieNode();
+            root = root.children[c - 'a'];
+            root.addToPQ(word);
+        }
+    }
 }
