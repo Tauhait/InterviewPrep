30e34b8d26561030bd60c1b7baf13fda6f94b52c
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-06-23 02:16:47 +0530

    Time: 605 ms (5.03%), Space: 56.9 MB (8.01%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
@@ -1,21 +1,69 @@
 class KthLargest {
-
-    private int k;
-    private Queue<Integer> minHeap = new PriorityQueue<>();
-    
-    public KthLargest(int k, int[] nums) {
-        this.k = k;    
-        for (int num : nums) {
-            add(num);
+    // insert a node into the BST
+    private Node insertNode(Node root, int num) {
+        if (root == null) {
+            return new Node(num, 1);
         }
+        if (root.val < num) {
+            root.right = insertNode(root.right, num);
+        } else {
+            root.left = insertNode(root.left, num);
+        }
+        root.cnt++;
+        return root;
+    }
+
+    private int searchKth(Node root, int k) {
+        // m = the size of right subtree
+        int m = root.right != null ? root.right.cnt : 0;
+        // root is the m+1 largest node in the BST
+        if (k == m + 1) {
+            return root.val;
+        }
+        if (k <= m) {
+            // find kth largest in the right subtree
+            return searchKth(root.right, k);
+        } else {
+            // find (k-m-1)th largest in the left subtree
+            return searchKth(root.left, k - m - 1);
+        }
+    } 
+    
+    private Node root;
+    private int m_k;
+
+    public KthLargest(int k, int[] nums) {
+        root = null;
+        for (int i = 0; i < nums.length; ++i) {
+            root = insertNode(root, nums[i]);
+        }
+        m_k = k;
     }
     
     public int add(int val) {
-        minHeap.add(val);
-        if (minHeap.size() > k) minHeap.poll();
-        return minHeap.peek();
+        root = insertNode(root, val);
+        return searchKth(root, m_k);
     }
 }
+
+class Node {    // the structure for the tree node
+    Node left;
+    Node right;
+    int val;
+    int cnt;    // the size of the subtree rooted at the node
+    public Node (int v, int c) {
+        left = null;
+        right = null;
+        val = v;
+        cnt = c;
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
 
 /**
  * Your KthLargest object will be instantiated and called as such:
