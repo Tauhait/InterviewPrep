325066fa3348441520ec9a090b00f9a667a57f70
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-06-23 09:15:43 +0530

    Time: 1 ms (98.85%), Space: 45 MB (14.66%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -13,17 +13,27 @@
  *     }
  * }
  */
+// since the height of a tree is always greater than or equal to 0
+// we use -1 as a flag to indicate if the subtree is not balanced
+/**
+But if you notice, with the top down approach we are calculating the height at each node Log N times. This is redundant. I tried adding a Map that would record the depth at each Node as we go down the tree, and during each subsequent height calculation we can access from the map cache. This route somehow added more overhead in time complexity... not sure why.
+
+The better approach would be to find the height from the bottom up.
+*/
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if(root == null) return true;
-        boolean isBalc = true;
-        if(!(Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1)) isBalc = false;
-        if(!isBalanced(root.left)) isBalc = false;
-        if(!isBalanced(root.right)) isBalc = false;
-        return isBalc;
+        return getHeight(root) != -1;
     }
-    private int getHeight(TreeNode node){
-        if(node == null) return 0;
-        return 1 + Math.max(getHeight(node.left), getHeight(node.right));
+    
+    private int getHeight(TreeNode node) {
+        if (node == null) return 0;
+
+        int left = getHeight(node.left);
+        int right = getHeight(node.right);
+
+        // left, right subtree is unbalanced or cur tree is unbalanced
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) return -1;
+
+        return Math.max(left, right) + 1;
     }
 }
