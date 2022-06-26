37a6c946980d3e404f9ca3dc436325e137717d4f
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-06-26 12:59:32 +0530

    Time: 3 ms (58.92%), Space: 64.5 MB (79.95%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,26 +1,24 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
-        //handle corner cases
-        int totalPoints = 0;
-        for(int point : cardPoints){
-            totalPoints += point;
+        int frontScore = 0;
+        int rearScore = 0;
+        int n = cardPoints.length;
+
+        for (int i = 0; i < k; i++) {
+            frontScore += cardPoints[i];
         }
-        if(k == cardPoints.length){
-            return totalPoints;
+
+        // take all k cards from the beginning
+        int maxScore = frontScore;
+
+        // take i from the beginning and k - i from the end
+        for (int i = k - 1; i >= 0; i--) {
+            rearScore += cardPoints[n - (k - i)];
+            frontScore -= cardPoints[i];
+            int currentScore = rearScore + frontScore;
+            maxScore = Math.max(maxScore, currentScore);
         }
-        int toFind = cardPoints.length - k;
-        int winLeft = 0, winRight;
-        int winSum = 0;
-        int miniSum = totalPoints;
-        
-        for(winRight = 0; winRight < cardPoints.length; winRight++){
-            winSum += cardPoints[winRight];
-            if((winRight - winLeft + 1) == toFind){
-                miniSum = Math.min(miniSum, winSum);
-                winSum -= cardPoints[winLeft];
-                winLeft++;
-            }
-        }
-        return totalPoints - miniSum;
+
+        return maxScore;
     }
 }
