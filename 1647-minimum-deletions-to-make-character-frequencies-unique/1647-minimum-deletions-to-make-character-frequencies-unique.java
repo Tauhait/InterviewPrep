c71ce92183abc7b1de4eb8b9a79a3c4eec1182ae
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-06-29 00:44:38 +0530

    Time: 12 ms (84.10%), Space: 55.1 MB (27.74%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,30 +1,24 @@
-class Solution {
-    
+class Solution {    
     public int minDeletions(String s) {
-        Map<Character, Integer> charFreqMap = new HashMap<Character, Integer>();//O(26)
-        PriorityQueue<Integer> sameCharFreqQ = new PriorityQueue<Integer>();//O(n)
-        Set<Integer> uniqueFreqSet = new HashSet<Integer>();//O(26)
+        // Store the frequency of each character
+        int[] frequency = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            frequency[s.charAt(i) - 'a']++;
+        }
         
-        //char freq calc step
-        for(char c : s.toCharArray()) charFreqMap.put(c, charFreqMap.getOrDefault(c, 0) + 1);
-        //find same and uniq freq values
-        for(Map.Entry<Character, Integer> entry : charFreqMap.entrySet()){
-            int val = entry.getValue();
-            if(uniqueFreqSet.contains(val)) sameCharFreqQ.add(val);
-            else uniqueFreqSet.add(val);
+        int deleteCount = 0;
+        // Use a set to store the frequencies we have already seen
+        HashSet<Integer> seenFrequencies = new HashSet<>();
+        for (int i = 0; i < 26; i++) {
+            // Keep decrementing the frequency until it is unique
+            while (frequency[i] > 0 && seenFrequencies.contains(frequency[i])) {
+                frequency[i]--;
+                deleteCount++;
+            }
+            // Add the newly occupied frequency to the set
+            seenFrequencies.add(frequency[i]);
         }
-        int ans = 0;
-        while(!sameCharFreqQ.isEmpty()){
-            int topFreqStart = sameCharFreqQ.poll();
-            int topFreqEnd = topFreqStart - 1;
-            while(topFreqEnd > 0 && uniqueFreqSet.contains(topFreqEnd)) topFreqEnd -= 1;
-            // System.out.println(topFreqStart + "," + topFreqEnd);            
-            ans += topFreqStart - topFreqEnd;
-            // System.out.println(ans);
-            if(topFreqEnd == 0) continue;
-            uniqueFreqSet.add(topFreqEnd);
-                       
-        }
-        return ans;
+        
+        return deleteCount;
     }
 }
