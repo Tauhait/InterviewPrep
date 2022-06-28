447a488d18d1f402e48f561f65559d0d1cf39dec
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-06-29 00:46:46 +0530

    Time: 13 ms (78.11%), Space: 54.8 MB (35.72%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -22,3 +22,23 @@ class Solution {
         return deleteCount;
     }
 }
+/**
+Here, NN is the length of the given string, and KK is the maximum possible number of distinct characters in s.
+
+Time complexity: O(N + K^2)O(N+K 
+2
+ )
+
+To store the frequencies, we need to traverse the string, which will take O(N)O(N) time. The maximum number of operations will occur when the frequencies for all the KK characters are the same. In that case, for the i_{th}i 
+th
+​
+  character, we would need to decrement the frequency by i - 1i−1 to make it unique. Hence the total number of operations would be 0 + 1 + 2 + .... + K - 1 = ((K - 1) * K )/20+1+2+....+K−1=((K−1)∗K)/2. Hence, the total time complexity is O(N + K^2)O(N+K 
+2
+ ).
+
+Space complexity: O(K)O(K)
+
+We need KK indices in the list frequency to store the frequencies. Also, there can be at most KK unique frequencies, and hence the space required for the HashSet seenFrequencies is O(K)O(K). Hence, the space complexity is equal to O(K)O(K).
+
+
+*/
