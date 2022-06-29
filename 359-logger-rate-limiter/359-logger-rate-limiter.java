36e585e42ae9059deb0fca02462dc54ada92ed15
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-06-29 11:53:42 +0530

    Time: 75 ms (11.50%), Space: 68.9 MB (5.27%) - LeetHub

--- 359-logger-rate-limiter/359-logger-rate-limiter.java
+++ 359-logger-rate-limiter/359-logger-rate-limiter.java
@@ -1,22 +1,39 @@
 class Logger {
-    private HashMap<String,Integer> lastLogged;
-    private final int THRESHOLD = 10;
-    
+    private Map<String, Integer> cacheOld;
+    private Map<String, Integer> cacheNew;
+    private int latest;
+
+    /** Initialize your data structure here. */
     public Logger() {
-        lastLogged = new HashMap<>();
+        this.cacheOld = new HashMap<String, Integer>();
+        this.cacheNew = new HashMap<String, Integer>();
+        
+        this.latest = 0;
     }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
     public boolean shouldPrintMessage(int timestamp, String message) {
-        Set<String> keys = new HashSet<String>(lastLogged.keySet());
-        for(String key : keys) {
-            if(timestamp - lastLogged.get(key) >= THRESHOLD) {
-                lastLogged.remove(key);
-            }
+        if(timestamp >= latest + 20){
+            cacheOld.clear();
+            cacheNew.clear();
+            latest = timestamp;
+        }else if(timestamp >= latest + 10){
+            cacheOld = new HashMap<>(cacheNew);
+            cacheNew.clear();
+            latest = timestamp;
         }
-        if((timestamp-lastLogged.getOrDefault(message, timestamp-(THRESHOLD+1))) >= THRESHOLD){
-            lastLogged.put(message, timestamp);
-            return true;
+        
+        if(cacheNew.containsKey(message)) return false;
+        
+        if(cacheOld.containsKey(message)){
+            int last = cacheOld.get(message);
+            if(last + 10 > timestamp) return false;
         }
-        return false;
+        
+        cacheNew.put(message, timestamp);
+        return true;
     }
 }
 
