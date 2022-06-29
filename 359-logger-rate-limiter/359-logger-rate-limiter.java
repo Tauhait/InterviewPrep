17f4d7dff28c801b348b953961055913fcf33e4e
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-06-29 11:56:29 +0530

    Time: 52 ms (51.35%), Space: 65.3 MB (48.18%) - LeetHub

--- 359-logger-rate-limiter/359-logger-rate-limiter.java
+++ 359-logger-rate-limiter/359-logger-rate-limiter.java
@@ -1,39 +1,29 @@
 class Logger {
-    private Map<String, Integer> cacheOld;
-    private Map<String, Integer> cacheNew;
-    private int latest;
-
+    private final HashMap<String, Integer> msgMap;
+    private final Object lock;
+	
     /** Initialize your data structure here. */
     public Logger() {
-        this.cacheOld = new HashMap<String, Integer>();
-        this.cacheNew = new HashMap<String, Integer>();
-        
-        this.latest = 0;
+    	msgMap = new HashMap<String, Integer>();
+    	lock = new Object();
     }
     
     /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
         If this method returns false, the message will not be printed.
         The timestamp is in seconds granularity. */
     public boolean shouldPrintMessage(int timestamp, String message) {
-        if(timestamp >= latest + 20){
-            cacheOld.clear();
-            cacheNew.clear();
-            latest = timestamp;
-        }else if(timestamp >= latest + 10){
-            cacheOld = new HashMap<>(cacheNew);
-            cacheNew.clear();
-            latest = timestamp;
-        }
-        
-        if(cacheNew.containsKey(message)) return false;
-        
-        if(cacheOld.containsKey(message)){
-            int last = cacheOld.get(message);
-            if(last + 10 > timestamp) return false;
-        }
-        
-        cacheNew.put(message, timestamp);
-        return true;
+        Integer ts = msgMap.get(message);
+        if (ts == null || timestamp - ts >= 10) {
+        	synchronized (lock) {
+        		Integer ts2 = msgMap.get(message);
+        		if (ts == null || timestamp - ts2 >= 10) {
+		            msgMap.put(message, timestamp);
+        			return true;
+        		}
+        	}
+         } 
+
+        return false;
     }
 }
 
