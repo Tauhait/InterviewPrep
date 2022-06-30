fe3add83916e43f9a00d8674dff58dc880e44034
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-06-30 11:03:06 +0530

    Time: 6 ms (99.48%), Space: 45.2 MB (96.15%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,21 +1,25 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if (strs.length == 0) return new ArrayList();
-        Map<String, List> ans = new HashMap<String, List>();
-        int[] count = new int[26];
-        for (String s : strs) {
-            Arrays.fill(count, 0);
-            for (char c : s.toCharArray()) count[c - 'a']++;
-
-            StringBuilder sb = new StringBuilder("");
-            for (int i = 0; i < 26; i++) {
-                sb.append('#');
-                sb.append(count[i]);
+        if(strs == null || strs.length == 0) return Collections.emptyList();
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s: strs){
+            //char type 0~127 is enough for constraint 0 <= strs[i].length <= 100
+            //char array to String is really fast, thanks @legendaryengineer
+            //You should use other data type when length of string is longer.
+            //E.g. Use byte (-128 to 127), short (-32,768 to 32,767),
+            //int. -2,147,483,648 to 2,147,483,647
+            char[] frequencyArr = new char[26];
+            for(int i = 0;i<s.length();i++){
+                frequencyArr[s.charAt(i) - 'a']++;
             }
-            String key = sb.toString();
-            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
-            ans.get(key).add(s);
+            //6 ms use char(0~127) array and new String(frequencyArr) method.
+            //17ms when use byte (-128 to 127) array and Arrays.toString(frequencyArr) method
+            //29ms when use int(-2,147,483,648 to 2,147,483,647) and Arrays.toString(frequencyArr) method
+            String key = new String(frequencyArr);
+            List<String> tempList = map.getOrDefault(key, new LinkedList<String>());
+            tempList.add(s);
+            map.put(key,tempList);
         }
-        return new ArrayList(ans.values());
+        return new LinkedList<>(map.values());
     }
 }
