58aed31c14bc17b024a7de17d418be86abd77adb
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-06-30 10:37:33 +0530

    Time: 3 ms (98.32%), Space: 45.9 MB (42.98%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -1,10 +1,36 @@
 class Solution {
     public int minMoves2(int[] nums) {
-        int ans = 0;
-        Arrays.sort(nums);
-        int middle = nums[nums.length/2];
-        for(int n : nums) ans += Math.abs(n - middle);
-        return ans;
+        int operations = 0, mid = quickSelect(nums, 0, nums.length-1, nums.length/2);
+        for (int num: nums) operations += Math.abs(mid - num);
+        return operations;
+    }
+    
+    private int quickSelect(int[] nums, int left, int right, int k) {
+        if (left == right) return nums[left];
+
+        int pIndex = new Random().nextInt(right - left + 1) + left;
+        pIndex = partition(nums, left, right, pIndex);
+
+        if (pIndex == k) return nums[k];
+        else if (pIndex < k) return quickSelect(nums, pIndex+1, right, k);
+        return quickSelect(nums, left, pIndex-1, k);
+    }
+
+    private int partition(int[] nums, int left, int right, int pIndex) {
+        int pivot = nums[pIndex];
+        swap(nums, pIndex, right);
+        pIndex = left;
+
+        for (int i=left; i<=right; i++) 
+            if (nums[i] <= pivot) swap(nums, i, pIndex++);
+
+        return pIndex - 1;
+    }
+
+    private void swap(int[] nums, int x, int y) {
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
     }
 }
 //try to figure out which number is closest ot every other number and then try to convert all other numbers to that number
