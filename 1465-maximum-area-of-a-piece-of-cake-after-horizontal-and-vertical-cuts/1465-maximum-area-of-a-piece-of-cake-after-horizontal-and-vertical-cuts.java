169dfb8f1f4ca57c92f34119d1d1fe9f5fab2faf
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-07-02 23:57:02 +0530

    Time: 26 ms (16.41%), Space: 60.9 MB (50.64%) - LeetHub

---
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
@@ -0,0 +1,37 @@
+class Solution {
+    // We will use long instead of int to prevent overflow
+    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+        // Start by sorting the inputs
+        Arrays.sort(horizontalCuts);
+        Arrays.sort(verticalCuts);
+        int n = horizontalCuts.length;
+        int m = verticalCuts.length;
+        
+        // Consider the edges first
+        long maxHeight = Math.max(horizontalCuts[0], h - horizontalCuts[n - 1]);
+        for (int i = 1; i < n; i++) {
+            // horizontalCuts[i] - horizontalCuts[i - 1] represents the distance between
+            // two adjacent edges, and thus a possible height
+            maxHeight = Math.max(maxHeight, horizontalCuts[i] - horizontalCuts[i - 1]);
+        }
+        
+        // Consider the edges first
+        long maxWidth = Math.max(verticalCuts[0], w - verticalCuts[m - 1]);
+        for (int i = 1; i < m; i++){
+            // verticalCuts[i] - verticalCuts[i - 1] represents the distance between
+            // two adjacent edges, and thus a possible width
+            maxWidth = Math.max(maxWidth, verticalCuts[i] - verticalCuts[i - 1]);
+        }
+
+        // Be careful of overflow, and don't forget the modulo!
+        return (int) ((maxWidth * maxHeight) % (1000000007));
+    }
+}
+/*
+1. Group all horizontal cuts and vertical cuts into cut intercepts and store in min heap (h*w)
+2. Add (0,0), (0,w), (h,0) and (h,w) to the min heap
+3. Add (0, w_i), (h_i, 0), (h_i, w) and (h, w_i) to the min heap
+4. Remove from min heap next cake
+5. Find area
+6. Calc maxArea
+*/
