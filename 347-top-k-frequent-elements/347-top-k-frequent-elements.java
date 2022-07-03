69e034bf35235d87a88760b28cfbdf8b334b7a04
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-07-03 23:22:08 +0530

    Time: 22 ms (35.93%), Space: 50.6 MB (48.17%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,32 +1,13 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        if(nums.length == k){
-            return nums;
-        }
-        Map<Integer, Integer> freqMap = new HashMap();
-        for(int n : nums){
-            freqMap.put(n, freqMap.getOrDefault(n, 0) + 1);
-        }
-        List<Integer>[] buckets = new ArrayList[nums.length + 1];
-        for(int i = 1; i <= nums.length; i++){
-            buckets[i] = new ArrayList<Integer>();
-        }
-        for(int key : freqMap.keySet()){
-            buckets[freqMap.get(key)].add(key);
-        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num : nums) map.put(num, map.getOrDefault(num, 0) + 1); 
         
-        int[] top = new int[k];
-        int j = nums.length;
-        for(int i = k - 1; i >= 0; ){
-            if(buckets[j].size() == 0){
-                j--;
-                continue;
-            }
-            List<Integer> bucketList = buckets[j--];
-            for(int b : bucketList){
-                top[i--] = b;
-            }
-        }
-        return top;
+        Queue<Integer> heap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
+        for(int key : map.keySet()) heap.add(key);
+        
+        int[] ans = new int[k];
+        for(int i = 0; i < k; i++) ans[i] = heap.poll();
+        return ans;
     }
 }
