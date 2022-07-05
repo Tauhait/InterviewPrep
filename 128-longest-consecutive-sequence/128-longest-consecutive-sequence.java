1c9af770aea62a4eb5a742377142a988fd5890c6
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-07-05 19:33:33 +0530

    Time: 77 ms (49.54%), Space: 81.7 MB (35.92%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,26 +1,67 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        Set<Integer> num_set = new HashSet<Integer>();
-        for (int num : nums) {
-            num_set.add(num);
+        UF uf = new UF(nums.length);
+        // Map val to index in nums
+        Map<Integer, Integer> valToIndex = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (valToIndex.containsKey(nums[i])) {
+                continue;
+            }
+            
+            if (valToIndex.containsKey(nums[i] - 1)) {
+                uf.union(i, valToIndex.get(nums[i] - 1));
+            }
+            
+            if (valToIndex.containsKey(nums[i] + 1)) {
+                uf.union(i, valToIndex.get(nums[i] + 1));
+            }
+            
+            valToIndex.put(nums[i], i);
         }
-
-        int longestStreak = 0;
-
-        for (int num : num_set) {
-            if (!num_set.contains(num-1)) {
-                int currentNum = num;
-                int currentStreak = 1;
-
-                while (num_set.contains(currentNum+1)) {
-                    currentNum += 1;
-                    currentStreak += 1;
-                }
-
-                longestStreak = Math.max(longestStreak, currentStreak);
+        
+        return uf.getLargetComponentSize();
+    }
+    
+    class UF {
+        private int[] parent;
+        private int[] size;
+        
+        public UF(int n) {
+            parent = new int[n];
+            size = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
             }
         }
-
-        return longestStreak;
+        
+        public void union(int x, int y) { // connected if consecutive
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                parent[rootX] = rootY;
+                size[rootY] += size[rootX];
+            }
+        };
+        
+        private int find(int x) {
+            if (parent[x] == x) {
+                return x;
+            }
+            
+            return parent[x] = find(parent[x]);
+        };
+        
+        public int getLargetComponentSize() {
+            int maxSize = 0;
+            for (int i = 0; i < parent.length; i++) {
+                if (parent[i] == i && size[i] > maxSize) {
+                    maxSize = size[i];
+                }
+            }
+            
+            return maxSize;
+        }
     }
 }
