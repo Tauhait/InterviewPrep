000c36eee8a86813d50dddf8c9d2b8e856d8b97d
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-07-07 18:13:14 +0530

    Time: 2 ms (88.75%), Space: 42.9 MB (25.44%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,34 +1,34 @@
 class Solution {
-    private Integer[][] memo;
+    private Boolean[][] memo;
     private String s1, s2, s3;
-    public int dp(int i, int j, int k) {
+    
+    public boolean dp(int i, int j, int k) {
         if (i < 0) {
-            return s2.substring(0, j + 1).equals(s3.substring(0, k + 1)) ? 1 : 0;
+            return s2.substring(0, j + 1).equals(s3.substring(0, k + 1));
         }
         if (j < 0) {
-            return s1.substring(0, i + 1).equals(s3.substring(0, k + 1)) ? 1 : 0;
+            return s1.substring(0, i + 1).equals(s3.substring(0, k + 1));
         }
-        if (memo[i][j] != null) {
-            return memo[i][j];
-        }
-        if (s3.charAt(k) == s1.charAt(i) && dp(i - 1, j, k - 1) == 1 || 
-            s3.charAt(k) == s2.charAt(j) && dp(i, j - 1, k - 1) == 1   ) {
-            memo[i][j] = 1;
-        }else {
-            memo[i][j] = 0;
-        }
+        if (memo[i][j] != null) return memo[i][j];
+        if (s3.charAt(k) == s1.charAt(i) && dp(i - 1, j, k - 1) || 
+            s3.charAt(k) == s2.charAt(j) && dp(i, j - 1, k - 1)) 
+             memo[i][j] = true;
+        else memo[i][j] = false;
         
         return memo[i][j];
     }
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1.length() + s2.length() != s3.length()) {
-            return false;
-        }
-        memo = new Integer[s1.length()][s2.length()];
+        if (s1.length() + s2.length() != s3.length()) return false;
+        
+        memo = new Boolean[s1.length()][s2.length()];
         this.s1 = s1;
         this.s2 = s2;
         this.s3 = s3;
 
-        return dp(s1.length() - 1, s2.length() - 1, s3.length() - 1) == 1 ? true : false;
+        return dp(s1.length() - 1, s2.length() - 1, s3.length() - 1);
     }
 }
+/*
+QtI
+1. Do s1 and s2 needs to exhaust completely?
+*/
