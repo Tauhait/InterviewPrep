5761bd913e1a77284eab06b5625c47afbae972f8
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-07-08 01:11:12 +0530

    Time: 3 ms (79.72%), Space: 54.8 MB (49.74%) - LeetHub

--- 498-diagonal-traverse/498-diagonal-traverse.java
+++ 498-diagonal-traverse/498-diagonal-traverse.java
@@ -1,64 +1,20 @@
 class Solution {
-    public int[] findDiagonalOrder(int[][] mat) {
-        int rows = mat.length;
-        int cols = mat[0].length;
-        Deque<int[]> deque = new ArrayDeque<int[]>();
-        int elements = mat.length * mat[0].length;
-        int[] ans = new int[elements];
-        int index = 0;
-        
-        deque.add(new int[]{0, 0});
-        
-        for(int i = 1; i < (rows+cols); i++){
-            int size = deque.size();
-            while(size-- > 0){
-                int[] indexes;
-                if(i % 2 == 1) {
-                    indexes = deque.pollFirst();
-                    // System.out.println("inside if "+indexes[0]+","+indexes[1]+"="+mat[indexes[0]][indexes[1]]);
-                    ans[index++] = mat[indexes[0]][indexes[1]];
-                    if(indexes[0]+1 < rows){
-                        if(deque.isEmpty()) deque.addLast(new int[]{indexes[0]+1, indexes[1]});//down
-                        else {
-                            int[] lastInsert = deque.peekLast();
-                            if(lastInsert[0] != indexes[0]+1) 
-                                deque.addLast(new int[]{indexes[0]+1, indexes[1]});//down
-                        }
-                    }
-                    if(indexes[1]+1 < cols){
-                        if(deque.isEmpty()) deque.addLast(new int[]{indexes[0], indexes[1]+1});//right
-                        else {
-                            int[] lastInsert = deque.peekLast();
-                            if(lastInsert[1] != indexes[1]+1) 
-                                deque.addLast(new int[]{indexes[0], indexes[1]+1});//right
-                        }
-                    }
-                }
-                else {
-                    indexes = deque.pollLast();
-                    // System.out.println("inside else "+indexes[0]+","+indexes[1]+"="+mat[indexes[0]][indexes[1]]);
-                    ans[index++] = mat[indexes[0]][indexes[1]];
-                    
-                   if(indexes[1]+1 < cols){
-                        if(deque.isEmpty()) deque.addFirst(new int[]{indexes[0], indexes[1]+1});//right
-                        else {
-                            int[] lastInsert = deque.peekLast();
-                            if(lastInsert[1] != indexes[1]+1) 
-                                deque.addFirst(new int[]{indexes[0], indexes[1]+1});//right
-                        }
-                    }
-                    if(indexes[0]+1 < rows){
-                        if(deque.isEmpty()) deque.addFirst(new int[]{indexes[0]+1, indexes[1]});//down
-                        else {
-                            int[] lastInsert = deque.peekLast();
-                            if(lastInsert[0] != indexes[0]+1) 
-                                deque.addFirst(new int[]{indexes[0]+1, indexes[1]});//down
-                        }
-                    }
-                }
-            }
-        }
-        return ans;
+    public int[] findDiagonalOrder(int[][] matrix) {
+        if (matrix.length == 0) return new int[0];
+        int r = 0, c = 0, m = matrix.length, n = matrix[0].length, arr[] = new int[m * n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = matrix[r][c];
+            if ((r + c) % 2 == 0) { // moving up
+                if      (c == n - 1) { r++; }
+                else if (r == 0)     { c++; }
+                else            { r--; c++; }
+            } else {                // moving down
+                if      (r == m - 1) { c++; }
+                else if (c == 0)     { r++; }
+                else            { r++; c--; }
+            }   
+        }   
+        return arr;
     }
 }
 //BFS
