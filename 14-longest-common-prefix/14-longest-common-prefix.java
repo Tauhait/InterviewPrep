6b04e4917630fa292e2ef73c5a7d2f5d8e297a55
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-07-09 01:11:39 +0530

    Time: 7 ms (25.06%), Space: 42.8 MB (20.80%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -1,11 +1,49 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-      if (strs == null || strs.length == 0) return "";
-      Arrays.sort(strs);
-      for(int i = 0; i < Math.min(strs[0].length(), strs[strs.length-1].length()); i++) {
-          if(strs[0].charAt(i) != strs[strs.length-1].charAt(i))
-              return strs[0].substring(0, i);
-      }
-      return strs[0];
+        if (strs == null || strs.length == 0) return "";
+        Trie trie = new Trie();
+        int minlen = 201;
+        String minStr = "";
+        for(String s : strs){
+            int slen = s.length();
+            if(slen < minlen){
+                minlen = slen;
+                minStr = s;
+            }
+            trie.insert(s);
+        }
+        return trie.lcp(minStr);
+    }
+}
+
+public class Trie {  
+    class TrieNode {
+        Map<Character, TrieNode> children = new HashMap<Character, TrieNode>();
+    }
+
+    private TrieNode root;
+    public Trie() {
+        root = new TrieNode();
+    }    
+    public void insert(String word) {
+        TrieNode node = root;
+        for(Character c : word.toCharArray()){
+            if(!node.children.containsKey(c)){
+                TrieNode newNode = new TrieNode();
+                node.children.put(c, newNode);
+            }
+            node = node.children.get(c);
+        }
+    }    
+    public String lcp(String prefix) {
+        TrieNode node = root;
+        int plen = prefix.length();
+        for(int i = 0; i < plen; i++){
+            char c = prefix.charAt(i);
+            if(!node.children.containsKey(c) || 
+               node.children.size() != 1) return prefix.substring(0,i);
+            node = node.children.get(c);
+        }
+        return prefix;
     }
 }
