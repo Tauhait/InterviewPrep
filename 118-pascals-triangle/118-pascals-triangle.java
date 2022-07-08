b449c3ef9b02ab7244fad8f0c1ddcb4bb8d24420
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-07-08 11:33:12 +0530

    Time: 1 ms (71.23%), Space: 41.7 MB (65.52%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -1,15 +1,12 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> pascal = new LinkedList<List<Integer>>();
-        for(int i = 0; i < numRows; i++){
-            List<Integer> cols = new LinkedList<Integer>();
-            if(i == 0) {
-                cols.add(1);
-                pascal.add(cols);
-                continue;
-            }
-            List<Integer> prevCol = new LinkedList<Integer>();
-            prevCol = pascal.get(i-1);
+        List<List<Integer>> pascal = new ArrayList<List<Integer>>();
+        pascal.add(new ArrayList<Integer>());
+        pascal.get(0).add(1);
+        
+        for(int i = 1; i < numRows; i++){
+            List<Integer> cols = new ArrayList<Integer>();
+            List<Integer> prevCol = pascal.get(i-1);
             cols.add(1);
             for(int j = 1; j < i; j++){
                 int sum = prevCol.get(j-1) + prevCol.get(j);
