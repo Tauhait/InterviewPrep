ec40de28543dd11d82dd6fcc0d53b1368dea046f
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-07-11 13:37:35 +0530

    Time: 2 ms (46.58%), Space: 43.1 MB (23.10%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -14,26 +14,31 @@
  * }
  */
 class Solution {
-    private final int delim = -101;
     public List<Integer> rightSideView(TreeNode root) {
-        Queue<TreeNode> bfsQ = new LinkedList<TreeNode>();
-        List<Integer> levelOrderTraversal = new ArrayList<Integer>();
-        List<Integer> ans = new ArrayList<Integer>();
-        if(root == null) return ans;
-        bfsQ.add(root);
-        while(!bfsQ.isEmpty()){
-            int size = bfsQ.size();
-            while(size-- > 0){
-                TreeNode child = bfsQ.poll();
-                levelOrderTraversal.add(child.val);
-                if(child.left != null) bfsQ.add(child.left);
-                if(child.right != null) bfsQ.add(child.right);
+        if (root == null) return new ArrayList<Integer>();
+        
+        ArrayDeque<TreeNode> queue = new ArrayDeque(){{ offer(root); }};
+        List<Integer> rightside = new ArrayList();
+        
+        while (!queue.isEmpty()) {
+            int levelLength = queue.size();
+
+            for(int i = 0; i < levelLength; ++i) {
+                TreeNode node = queue.poll();
+                // if it's the rightmost element
+                if (i == levelLength - 1) {
+                    rightside.add(node.val);    
+                }
+
+                // add child nodes in the queue
+                if (node.left != null) {
+                    queue.offer(node.left);    
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
             }
-            levelOrderTraversal.add(delim);
         }
-        for(int i = 0; i < levelOrderTraversal.size(); i++){
-            if(levelOrderTraversal.get(i) == delim) ans.add(levelOrderTraversal.get(i-1));
-        }
-        return ans;
+        return rightside;
     }
 }
