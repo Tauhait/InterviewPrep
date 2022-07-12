855dfe2e5757398ee5c8ec3c075b73309fbf533d
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-07-13 00:40:08 +0530

    Time: 163 ms (54.02%), Space: 42 MB (43.65%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.java
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -1,61 +1,29 @@
-import java.util.HashMap;
-import java.util.Collections;
-
 class Solution {
-    public List<Integer> nums;
-    public int[] sums;
-    public int possibleSquareSide;
-
-    public Solution() {
-        this.sums = new int[4];
-    }
-
-    // Depth First Search function.
-    public boolean dfs(int index) {
-
-        // If we have exhausted all our matchsticks, check if all sides of the square are of equal length
-        if (index == this.nums.size()) {
-            return sums[0] == sums[1] && sums[1] == sums[2] && sums[2] == sums[3];
-        }
-
-        // Get current matchstick.
-        int element = this.nums.get(index);
-
-        // Try adding it to each of the 4 sides (if possible)
-        for(int i = 0; i < 4; i++) {
-            if (this.sums[i] + element <= this.possibleSquareSide) {
-                this.sums[i] += element;
-                if (this.dfs(index + 1)) {
-                    return true;
-                }
-                this.sums[i] -= element;
-            }
-        }
-
-        return false;
-    }
-
     public boolean makesquare(int[] nums) {
-        // Empty matchsticks.
-        if (nums == null || nums.length == 0) {
+        if (nums == null || nums.length < 4) return false;
+        
+        int sum = 0;
+        
+        for (int num : nums) sum += num;
+        
+        if (sum % 4 != 0) return false;
+        
+        Arrays.sort(nums);
+        
+        return dfs(nums, new int[4], nums.length - 1, sum / 4);
+    }
+
+    private boolean dfs(int[] nums, int[] sums, int index, int target) {
+        if (index == -1) {
+            if (sums[0] == target && sums[1] == target && sums[2] == target) return true;
             return false;
         }
-
-        // Find the perimeter of the square (if at all possible)
-        int L = nums.length;
-        int perimeter = 0;
-        for(int i = 0; i < L; i++) {
-            perimeter += nums[i];
+        for (int i = 0; i < 4; i++) {
+            if (sums[i] + nums[index] > target) continue;
+            sums[i] += nums[index];
+            if (dfs(nums, sums, index -1, target)) return true;
+            sums[i] -= nums[index];
         }
-
-        this.possibleSquareSide =  perimeter / 4;
-        if (this.possibleSquareSide * 4 != perimeter) {
-            return false;
-        }
-
-        // Convert the array of primitive int to ArrayList (for sorting).
-        this.nums = Arrays.stream(nums).boxed().collect(Collectors.toList());
-        Collections.sort(this.nums, Collections.reverseOrder());
-        return this.dfs(0);
+        return false;
     }
 }
