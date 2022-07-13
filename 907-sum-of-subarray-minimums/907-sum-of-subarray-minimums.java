89367db1cfe63f5aa1272930258a74c7aa0fe04c
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-07-13 23:32:18 +0530

    Time: 106 ms (46.03%), Space: 67.7 MB (61.21%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.java
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.java
@@ -1,27 +1,18 @@
 class Solution {
-    private final long MOD = 1_000_000_007;
-    
-    public int sumSubarrayMins(int[] arr) {
-        Stack<Integer> monoStk = new Stack<Integer>();
-        int[] leftDist = new int[arr.length];
-        int[] rightDist = new int[arr.length];
-        long sum = 0;
-        
-        for(int i = 0; i < arr.length; i++){
-            while(!monoStk.isEmpty() && arr[i] <= arr[monoStk.peek()]) monoStk.pop();
-            leftDist[i] = monoStk.isEmpty() ? i+1 : i-monoStk.peek();
-            monoStk.push(i);
+    public int sumSubarrayMins(int[] A) {
+        Stack<Integer> s = new Stack<>();
+        int[] dp = new int[A.length];
+        int total = 0;
+        for (int i = 0; i < A.length; i++) {
+            while (!s.isEmpty() && A[s.peek()] > A[i]) 
+                s.pop();
+            if (s.isEmpty()) 
+                dp[i] = (i + 1) * A[i];
+            else 
+                dp[i] = dp[s.peek()] + (i - s.peek()) * A[i];
+            total = (total + dp[i] ) % 1000000007;
+            s.push(i);
         }
-        monoStk.clear();
-        
-        for(int i = arr.length-1; i >= 0; i--){
-            while(!monoStk.isEmpty() && arr[i] < arr[monoStk.peek()]) monoStk.pop();            
-            rightDist[i] = monoStk.isEmpty() ? arr.length-i : monoStk.peek()-i;
-            monoStk.push(i);
-        }
-        
-        for(int i = 0; i < arr.length; i++) sum = (sum + (long)arr[i]*leftDist[i]*rightDist[i]) % MOD;
-        
-        return (int)sum;
+        return total;
     }
 }
