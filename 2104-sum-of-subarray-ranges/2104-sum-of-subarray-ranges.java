fa1c52bd4b03a5b58ff315ca6cf113ad7cf5b5ed
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-07-13 17:24:04 +0530

    Time: 46 ms (13.17%), Space: 48.2 MB (13.03%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,72 +1,54 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
         int n = nums.length;
-        long sum=0;
+        long sum = 0;
         Stack<Integer> st = new Stack<>();
-        int[] minPrev = new int[n];
-        int[] minNext = new int[n];
-        int[] maxPrev = new int[n];
-        int[] maxNext = new int[n];
+        int[] prev = new int[n];
+        int[] next = new int[n];
         
-        Arrays.fill(minPrev , -1);
-        Arrays.fill(minNext , n);
-        Arrays.fill(maxPrev , -1 );
-        Arrays.fill(maxNext , n);
+        Arrays.fill(prev , -1);
+        Arrays.fill(next , n);
         
+        for(int i = 0; i < n; i++){
+            while(!st.isEmpty() && nums[st.peek()] >= nums[i]) st.pop();
+            if(!st.isEmpty()) prev[i] = st.peek();
+            st.push(i);
+        }        
+        st.clear();
         
-        for(int i=0;i<n;i++){
-            while(!st.isEmpty() && nums[st.peek()] >= nums[i]){
-                st.pop();
-            }
-            if(!st.isEmpty()){
-                minPrev[i] = st.peek();
-            }
-            
+        for(int i = n-1; i >= 0; i--){
+            while(!st.isEmpty() && nums[st.peek()] > nums[i]) st.pop();
+            if(!st.isEmpty()) next[i] = st.peek();
             st.push(i);
         }
-        
-        st = new Stack<>();
-        for(int i=n-1;i>=0;i--){
-            while(!st.isEmpty() && nums[st.peek()] > nums[i]){
-                st.pop();
-            }
-            if(!st.isEmpty()){
-                minNext[i] = st.peek();
-            }
-            st.push(i);
+        st.clear();
+        for(int i = 0; i < n; i++){
+            long leftMin = i-prev[i];
+            long rightMin = next[i]-i;
+            sum -= leftMin*rightMin*nums[i];
         }
         
-        st = new Stack<>();
-        for(int i=0 ;i<n ;i++){
-            while(!st.isEmpty() && nums[st.peek()] <= nums[i]){
-                st.pop();
-            }
-            if(!st.isEmpty()){
-                maxPrev[i] = st.peek();
-            }
+        Arrays.fill(prev , -1);
+        Arrays.fill(next , n);
+        
+        for(int i = 0 ; i < n ; i++){
+            while(!st.isEmpty() && nums[st.peek()] <= nums[i]) st.pop();
+            if(!st.isEmpty()) prev[i] = st.peek();
             st.push(i);
         }
+        st.clear();
         
-        st = new Stack<>();
-        for(int i=n-1 ; i>=0;i--){
-            while(!st.isEmpty() && nums[st.peek()] < nums[i]){
-                st.pop();
-            }
-            
-            if(!st.isEmpty()){
-                maxNext[i] = st.peek();
-            }
-            
+        for(int i = n-1 ; i >= 0; i--){
+            while(!st.isEmpty() && nums[st.peek()] < nums[i]) st.pop();            
+            if(!st.isEmpty()) next[i] = st.peek();            
             st.push(i);
         }
         
         
-        for(int i=0 ;i <n;i++){
-            long leftMin = i-minPrev[i];
-            long rightMin = minNext[i]-i;
-            long leftMax = i-maxPrev[i];
-            long rightMax = maxNext[i]-i;
-            sum+= (leftMax*rightMax - leftMin*rightMin)*nums[i];
+        for(int i = 0; i < n; i++){
+            long leftMax = i-prev[i];
+            long rightMax = next[i]-i;
+            sum += leftMax*rightMax*nums[i];
         }
         
         return sum;
