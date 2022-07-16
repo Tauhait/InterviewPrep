43e0ebbbf4b24b69edbc0c271f7f20985ebd1a5c
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-07-16 12:09:26 +0530

    Time: 18 ms (26.25%), Space: 43.3 MB (25.42%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.java
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.java
@@ -1,20 +1,35 @@
-class Solution {
-  int M = 1000000007;
 
-  public int findPaths(int m, int n, int N, int i, int j) {
-    int[][][] memo = new int[m][n][N + 1];
-    for (int[][] l : memo) for (int[] sl : l) Arrays.fill(sl, -1);
-    return findPaths(m, n, N, i, j, memo);
-  }
-
-  public int findPaths(int m, int n, int N, int i, int j, int[][][] memo) {
-    if (i == m || j == n || i < 0 || j < 0) return 1;
-    if (N == 0) return 0;
-    if (memo[i][j][N] >= 0) return memo[i][j][N];
-    memo[i][j][N] = (
-        (findPaths(m, n, N - 1, i - 1, j, memo) + findPaths(m, n, N - 1, i + 1, j, memo)) % M +
-        (findPaths(m, n, N - 1, i, j - 1, memo) + findPaths(m, n, N - 1, i, j + 1, memo)) % M
-    ) % M;
-    return memo[i][j][N];
-  }
+public class Solution {
+    public int findPaths(int m, int n, int N, int i, int j) {
+        if (N <= 0) return 0;
+        
+        final int MOD = 1000000007;
+        int[][] count = new int[m][n];
+        count[i][j] = 1;
+        int result = 0;
+        
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        
+        for (int step = 0; step < N; step++) {
+            int[][] temp = new int[m][n];
+            for (int r = 0; r < m; r++) {
+                for (int c = 0; c < n; c++) {
+                    for (int[] d : dirs) {
+                        int nr = r + d[0];
+                        int nc = c + d[1];
+                        if (nr < 0 || nr >= m || nc < 0 || nc >= n) {
+                            result = (result + count[r][c]) % MOD;
+                        }
+                        else {
+                            //temp[][] stores the k-th move while count[][] stores the k-1 th.
+                            temp[nr][nc] = (temp[nr][nc] + count[r][c]) % MOD;
+                        }
+                    }
+                }
+            }
+            count = temp;
+        }
+        
+        return result;
+    }
 }
