80edb34f98cd118dc22961df585f67d5733ec7b0
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-07-18 14:30:24 +0530

    Time: 126 ms (38.38%), Space: 75 MB (40.83%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.java
+++ 472-concatenated-words/472-concatenated-words.java
@@ -1,37 +1,52 @@
 public class Solution {
-    //Bottom Up Approach
-    public static List<String> findAllConcatenatedWordsInADict(String[] words) {
-        List<String> result = new ArrayList<>();
-        Set<String> preWords = new HashSet<>();
-        Arrays.sort(words, new Comparator<String>() {
-            public int compare (String s1, String s2) {
-                return s1.length() - s2.length();
-            }
-        });
-        
-        for (int i = 0; i < words.length; i++) {
-            if (canForm(words[i], preWords)) {
-                result.add(words[i]);
-            }
-            preWords.add(words[i]);
-        }
-        
-        return result;
-    }
-	
-    private static boolean canForm(String word, Set<String> dict) {
-        if (dict.isEmpty()) return false;
-        boolean[] dp = new boolean[word.length() + 1];
-        dp[0] = true;
-        for (int i = 1; i <= word.length(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (!dp[j]) continue;
-                if (dict.contains(word.substring(j, i))) {
-                    dp[i] = true;
-                    break;
-                }
-            }
-        }
-        return dp[word.length()];
+    //Top Down
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+     //sort the array in asc order of word length, since longer words are formed by shorter words.
+     Arrays.sort(words, (a,b) -> a.length() - b.length());
+
+      List<String> result = new ArrayList<>();
+
+      //list of shorter words 
+      HashSet<String> preWords = new HashSet<>();
+
+      for(int i=0; i< words.length; i++){
+          //Word Break-I problem.
+          if(topDown(words[i], preWords, 0, new Boolean[words[i].length()])) {
+              result.add(words[i]);
+          }
+          preWords.add(words[i]);
+      }
+      return result;
+     }
+
+    private boolean topDown(String s, HashSet<String> wordDict, int startIndex, Boolean[] memo) {
+     if(wordDict.isEmpty()) {
+         return false;
+     }
+// if we reach the beyond the string, then return true
+// s = "leetcode" when "code" is being checked in the IF() of the loop, 
+// we reach endIndex == s.length(), 
+// and wordDict.contains("code") => true and 
+// topDown(s, wordDict, endIndex, memo) needs to return true. 
+     if(startIndex == s.length()) {
+         return true;
+     }
+
+ // memo[i] = true means => that the substring from index i can be segmented. 
+ // memo[startIndex] means => wordDict contains substring from startIndex and 
+ // it can be segemented.
+     if(memo[startIndex] != null) { //Boolean[] array's default value is "null"
+         return memo[startIndex];
+     }
+
+     for(int endIndex = startIndex + 1; endIndex <= s.length(); endIndex++) {
+         if(wordDict.contains(s.substring(startIndex, endIndex)) && 
+            topDown(s, wordDict, endIndex, memo)) {
+             memo[startIndex] = true;
+             return true;
+         }
+     }
+     memo[startIndex] = false;
+     return false;
     }
 }
