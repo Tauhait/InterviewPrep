90b85ed8d9d922f34100c058721b7436e29cfd47
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-07-31 10:14:20 +0530

    Time: 3 ms (69.54%), Space: 76.5 MB (80.02%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,13 +1,13 @@
-public class Solution {
-    public int maxProfit(int prices[]) {
-        int minprice = Integer.MAX_VALUE;
-        int maxprofit = 0;
-        for (int i = 0; i < prices.length; i++) {
-            if (prices[i] < minprice)//buy if least price encountered till now 
-                minprice = prices[i];
-            else if (prices[i] - minprice > maxprofit)//sell if maxprofit till now
-                maxprofit = prices[i] - minprice;
+class Solution {
+    public int maxProfit(int[] prices) {
+        int largestDiff = 0;
+        int minSoFar = Integer.MAX_VALUE;
+        for(int buyDay = 0; buyDay < prices.length; buyDay++){
+            minSoFar = Math.min(minSoFar, prices[buyDay]);
+            largestDiff = Math.max(largestDiff, prices[buyDay] - minSoFar);
         }
-        return maxprofit;
+        return largestDiff;
     }
 }
+//BF: Evaluate all possible days for buying and selling which will cost O(n*n)
+//kadane's algo O(n)
