e2e65b966192c3ad949c953dd575294a0168abb3
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-08-02 23:44:17 +0530

    Time: 1 ms (92.44%), Space: 58.7 MB (6.01%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,64 +1,61 @@
-class MyHeapNode {
-
-  int row;
-  int column;
-  int value;
-
-  public MyHeapNode(int v, int r, int c) {
-    this.value = v;
-    this.row = r;
-    this.column = c;
-  }
-
-  public int getValue() {
-    return this.value;
-  }
-
-  public int getRow() {
-    return this.row;
-  }
-
-  public int getColumn() {
-    return this.column;
-  }
-}
-
-class MyHeapComparator implements Comparator<MyHeapNode> {
-  public int compare(MyHeapNode x, MyHeapNode y) {
-    return x.value - y.value;
-  }
-}
-
 class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        if (matrix == null || k <= 0) {
+            throw new IllegalArgumentException("Input is invalid");
+        }
 
-  public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        if (k > n * n) {
+            throw new IllegalArgumentException("k is greater than " +
+                                             "number of elements in matrix");
+        }
+        if (k == 1) {
+            return matrix[0][0];
+        }
+        if (k == n * n) {
+            return matrix[n - 1][n - 1];
+        }
 
-    int N = matrix.length;
+        int start = matrix[0][0];
+        int end = matrix[n - 1][n - 1];
 
-    PriorityQueue<MyHeapNode> minHeap =
-        new PriorityQueue<MyHeapNode>(Math.min(N, k), new MyHeapComparator());
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            int[] smallLargePair = { start, end };
+            int count = countLessThanAndEqual(matrix, mid, smallLargePair);
+            if (count == k) {
+                return smallLargePair[0];
+            }
+            if (count < k) {
+                start = smallLargePair[1]; // search higher
+            } else {
+                end = smallLargePair[0]; // search lower
+            }
+        }
 
-    // Preparing our min-heap
-    for (int r = 0; r < Math.min(N, k); r++) {
-
-      // We add triplets of information for each cell
-      minHeap.offer(new MyHeapNode(matrix[r][0], r, 0));
+        return start;
     }
 
-    MyHeapNode element = minHeap.peek();
-    while (k-- > 0) {
-
-      // Extract-Min
-      element = minHeap.poll();
-      int r = element.getRow(), c = element.getColumn();
-
-      // If we have any new elements in the current row, add them
-      if (c < N - 1) {
-
-        minHeap.offer(new MyHeapNode(matrix[r][c + 1], r, c + 1));
-      }
+    private int countLessThanAndEqual(int[][] matrix, int mid, int[] smallLargePair) {
+        int count = 0;
+        int n = matrix.length;
+        int row = 0;
+        int col = n - 1;
+        while (row < n && col >= 0) {
+            if (matrix[row][col] > mid) {
+                // as matrix[row][col] is bigger than the mid, let's keep track of the
+                // smallest number greater than the mid
+                smallLargePair[1] = Math.min(smallLargePair[1], matrix[row][col]);
+                col--;
+            } else {
+                // as matrix[row][col] is less than or equal to the mid, 
+                //let's keep track of the
+                // biggest number less than or equal to the mid
+                smallLargePair[0] = Math.max(smallLargePair[0], matrix[row][col]);
+                row++;
+                count += col + 1;
+            }
+        }
+        return count;
     }
-
-    return element.getValue();
-  }
 }
