7693219bcf88aac99a18bbad8b6a66b93bed2e0f
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-08-04 11:25:18 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (52.78%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.java
+++ 858-mirror-reflection/858-mirror-reflection.java
@@ -10,15 +10,21 @@ https://www.youtube.com/watch?v=TEh0Fcu5Nok
 class Solution {
     public int mirrorReflection(int p, int q) {
         int m = 1, n = 1;
-        while(p*m != q*n){
+        int gcd = gcd(p,q);
+        p /= gcd;
+        q /= gcd;
+        while (p*m != q*n){
             n++;
             m = (q*n)/p;
         }
-        
         //p*m == q*n
 
         if(n % 2 == 0) return 2;
         else if(m % 2 == 1) return 1;
         else return 0;
+    }
+    private int gcd(int p, int q){
+        if(q == 0) return p;
+        return gcd(q, p % q);
     }
 }
