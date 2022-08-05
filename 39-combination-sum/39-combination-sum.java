ed75fcc9c56d189a78c85fc9e1a335f775acf1bb
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-08-05 23:03:18 +0530

    Time: 17 ms (9.89%), Space: 50.9 MB (7.88%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,19 +1,20 @@
-class Solution {
-    
-    private void generateCombinations(int start, int sum, int target, int[] nums,
-                                            List<List<Integer>> ans, List<Integer> curr){
-        if(sum > target) return;
-        if(sum == target) {
-            ans.add(new ArrayList<>(curr));
+class Solution {    
+    private void generateCombinations(int start, int remain, int[] nums,
+                                            List<List<Integer>> ans, LinkedList<Integer> curr){
+        if(remain < 0) return;
+        if(remain == 0) {
+            ans.add(new LinkedList<>(curr));
             return;
         }
         
+        //pos is initialized at start to avoid duplicity of results and 
+        //also enable repetition of candidates in the current sum to match the target
         for(int pos = start; pos < nums.length; pos++){
             curr.add(nums[pos]);
-            sum += nums[pos];
-            generateCombinations(pos, sum, target, nums, ans, curr);
-            curr.remove(curr.size()-1);
-            sum -= nums[pos];
+            // sum += nums[pos];
+            generateCombinations(pos, remain-nums[pos], nums, ans, curr);
+            curr.removeLast();
+            // sum -= nums[pos];
         }
     } 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
@@ -21,9 +22,9 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         
         for(int pos = 0; pos < candidates.length; pos++){
-            List<Integer> curr = new ArrayList<>();
+            LinkedList<Integer> curr = new LinkedList<>();
             curr.add(candidates[pos]);
-            generateCombinations(pos, candidates[pos], target, candidates, ans, curr);
+            generateCombinations(pos, target-candidates[pos], candidates, ans, curr);
         }
         return ans;
     }
