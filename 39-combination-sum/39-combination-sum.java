2100921f78c7a4e6b355544a83f500b2b2f6fabd
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-08-05 23:07:04 +0530

    Time: 12 ms (18.70%), Space: 50.8 MB (8.10%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -3,7 +3,7 @@ class Solution {
                                             List<List<Integer>> ans, LinkedList<Integer> curr){
         if(remain < 0) return;
         if(remain == 0) {
-            ans.add(new LinkedList<>(curr));
+            ans.add(new ArrayList<>(curr));
             return;
         }
         
@@ -11,21 +11,16 @@ class Solution {
         //also enable repetition of candidates in the current sum to match the target
         for(int pos = start; pos < nums.length; pos++){
             curr.add(nums[pos]);
-            // sum += nums[pos];
             generateCombinations(pos, remain-nums[pos], nums, ans, curr);
             curr.removeLast();
-            // sum -= nums[pos];
         }
     } 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> ans = new ArrayList<>();
-        
-        for(int pos = 0; pos < candidates.length; pos++){
-            LinkedList<Integer> curr = new LinkedList<>();
-            curr.add(candidates[pos]);
-            generateCombinations(pos, target-candidates[pos], candidates, ans, curr);
-        }
+
+        LinkedList<Integer> curr = new LinkedList<>();
+        generateCombinations(0, target, candidates, ans, curr);
         return ans;
     }
 }
