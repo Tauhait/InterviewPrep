ce1aaafd8e2249971ce215528f14f865b487b502
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-08-06 19:26:13 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (5.60%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.java
+++ 458-poor-pigs/458-poor-pigs.java
@@ -1,6 +1,6 @@
 class Solution {
     public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
         int periods = minutesToTest/minutesToDie;
-        return (int)Math.ceil(Math.log10(buckets)/Math.log10(periods+1));
+        return (int)Math.ceil(Math.log(buckets)/Math.log(periods+1));
     }
 }
