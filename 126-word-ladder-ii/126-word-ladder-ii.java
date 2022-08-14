6d27f335521b9d13a590ad78e364bbd1e122de08
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-08-14 21:37:23 +0530

    Time: 55 ms (15.73%), Space: 52.8 MB (21.93%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.java
+++ 126-word-ladder-ii/126-word-ladder-ii.java
@@ -1,106 +1,68 @@
 class Solution {
-    Map<String, List<String>> adjList = new HashMap<String, List<String>>();
-    List<String> currPath = new ArrayList<String>();
-    List<List<String>> shortestPaths = new ArrayList<List<String>>();
-    
-    private List<String> findNeighbors(String word, Set<String> wordList) {
-        List<String> neighbors = new ArrayList<String>();
-        char charList[] = word.toCharArray();
-        
-        for (int i = 0; i < word.length(); i++) {
-            char oldChar = charList[i];   
-            
-            // replace the i-th character with all letters from a to z except the original character
-            for (char c = 'a'; c <= 'z'; c++) {
-                charList[i] = c;
-                
-                // skip if the character is same as original or if the word is not present in the wordList
-                if (c == oldChar || !wordList.contains(String.valueOf(charList))) {
-                    continue;
-                }
-                neighbors.add(String.valueOf(charList));
-            }
-            charList[i] = oldChar;
+    Set<String> set = new HashSet();
+    String beginWord, endWord;
+    Map<String, Integer> dist = new HashMap();
+    List<List<String>> res;
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        this.beginWord = beginWord;
+        this.endWord = endWord;
+        this.res = new ArrayList();
+        for (String word : wordList) {
+            set.add(word);
         }
-        return neighbors;
+        short_path();
+        if (dist.get(endWord) == null) return res;
+        List<String> path = new ArrayList();
+        path.add(endWord);
+        dfs(endWord, path);
+        return res;
     }
     
-    private void backtrack(String source, String destination) {
-        // store the path if we reached the endWord
-        if (source.equals(destination)) {
-            List<String> tempPath = new ArrayList<String>(currPath);
-            Collections.reverse(tempPath);
-            shortestPaths.add(tempPath);
+    private void short_path() {
+        Queue<String> q = new LinkedList();
+        q.offer(beginWord);
+        dist.put(beginWord, 0);
+        while(q.size() > 0) {
+            String cur = q.poll();
+            if (cur.equals(endWord) ) break;
+            char[] charCur = cur.toCharArray();
+            for (int i = 0; i < cur.length(); i++) {
+                char c = charCur[i];
+                for (char j = 'a'; j <= 'z'; j++) {
+                    charCur[i] = j;
+                    String s = new String(charCur);
+                    if (set.contains(s) && dist.get(s) == null) {
+                        dist.put(s, dist.get(cur) + 1);
+                        q.offer(s);
+                    }
+                    
+                }
+                charCur[i] = c;
+            }
         }
-        
-        if (!adjList.containsKey(source)) {
+    }
+    
+    private void dfs(String word, List<String> path) {
+        if (word.equals(beginWord)) {
+            List list = new ArrayList(path);
+            Collections.reverse(list);
+            res.add(list);
             return;
         }
-        
-        for (int i = 0; i < adjList.get(source).size(); i++) {
-            currPath.add(adjList.get(source).get(i));
-            backtrack(adjList.get(source).get(i), destination);
-            currPath.remove(currPath.size() - 1);
-        }
-    }
-    
-    private void bfs(String beginWord, String endWord, Set<String> wordList) {
-        Queue<String> q = new LinkedList<>();
-        q.add(beginWord);
-        
-        // remove the root word which is the first layer in the BFS
-        if (wordList.contains(beginWord)) {
-            wordList.remove(beginWord);
-        }
-        
-        Map<String, Integer> isEnqueued = new HashMap<String, Integer>();
-        isEnqueued.put(beginWord, 1);
-        
-        while (q.size() > 0)  {
-             // visited will store the words of current layer
-            List<String> visited = new ArrayList<String>();
-            
-            for (int i = q.size() - 1; i >= 0; i--) {
-                String currWord = q.peek(); 
-                q.remove();
-
-                // findNeighbors will have the adjacent words of the currWord
-                List<String> neighbors = findNeighbors(currWord, wordList);
-                for (String word : neighbors) {
-                    visited.add(word);
-                    
-                    if (!adjList.containsKey(word)) {
-                        adjList.put(word, new ArrayList<String>());
-                    }
-                    
-                    // add the edge from word to currWord in the list
-                    adjList.get(word).add(currWord);
-                    if (!isEnqueued.containsKey(word)) {
-                        q.add(word);
-                        isEnqueued.put(word, 1);
-                    }
+        char[] charCur = word.toCharArray();
+        for (int i = 0; i < word.length(); i++) {
+            char c = charCur[i];
+            for (char j = 'a'; j <= 'z'; j++) {
+                charCur[i] = j;
+                String s = new String(charCur);
+                if (dist.get(s) != null && dist.get(s) + 1 == dist.get(word)) {
+                    path.add(s);
+                    dfs(s, path);
+                    path.remove(path.size() - 1);
                 }
+                    
             }
-             // removing the words of the previous layer
-            for (int i = 0; i < visited.size(); i++) {
-                if (wordList.contains(visited.get(i))) {
-                    wordList.remove(visited.get(i));
-                }
-            }
+            charCur[i] = c;
         }
-    }
-    
-    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-        // copying the words into the set for efficient deletion in BFS
-        Set<String> copiedWordList = new HashSet<>(wordList);
-        // build the DAG using BFS
-        bfs(beginWord, endWord, copiedWordList);
-        
-        // every path will start from the endWord
-        currPath.add(endWord);
-        // traverse the DAG to find all the paths between endWord and beginWord
-        backtrack(endWord, beginWord);
-        
-        return shortestPaths;
     }
 }
