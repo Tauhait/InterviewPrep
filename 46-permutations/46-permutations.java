f74fd59602bd52e3c5c19b17cdb798e1bd00506a
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-08-18 22:44:38 +0530

    Time: 2 ms (77.04%), Space: 42.2 MB (99.35%) - LeetHub

---
+++ 46-permutations/46-permutations.java
@@ -0,0 +1,24 @@
+class Solution {
+    private List<List<Integer>> ans;
+    
+    private void backtrack(int[] nums, List<Integer> currentPermute, Set<Integer> permuteSet){
+        if(currentPermute.size() == nums.length) {
+            ans.add(new ArrayList<Integer>(currentPermute));
+            return;
+        }
+        
+        for(int i = 0; i < nums.length; i++){ 
+            if(permuteSet.contains(nums[i])) continue;
+            currentPermute.add(nums[i]);
+            permuteSet.add(nums[i]);
+            backtrack(nums, currentPermute, permuteSet);
+            int valRemove = currentPermute.remove(currentPermute.size()-1);
+            permuteSet.remove(valRemove);
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        ans = new ArrayList<List<Integer>>();
+        backtrack(nums, new ArrayList<Integer>(), new HashSet<Integer>());
+        return ans;
+    }
+}
