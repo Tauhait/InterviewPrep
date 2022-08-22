7049df5099e73624d56c15f1d534cf613637c662
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-08-22 11:35:33 +0530

    Time: 1 ms (100.00%), Space: 42.3 MB (5.32%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -1,12 +1,6 @@
 class Solution {
-    public boolean isPowerOfFour(int n) {
-        int ones = 0, zeros = 0;
-        while(n > 0){
-            int and1 = n & 1;
-            if(and1 == 1) ones++;
-            else zeros++;
-            n >>= 1;
-        }
-        return (ones == 1 && zeros % 2 == 0);
+    public boolean isPowerOfFour(int num) {
+        return (num > 0) && ((num & (num - 1)) == 0) && ((num & 0xaaaaaaaa) == 0);
     }
+    //How to check if the number is a power of two : x > 0 and x & (x - 1) == 0
 }
