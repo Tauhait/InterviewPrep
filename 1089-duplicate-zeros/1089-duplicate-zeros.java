b2519ef1ee3b9e19ea307e9359d060a57ade613a
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-08-25 15:22:03 +0530

    Time: 2 ms (66.12%), Space: 45.9 MB (54.71%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,25 +1,10 @@
 class Solution {
-    public void duplicateZeros(int[] arr) {
-        int n = arr.length;
-        int[] countZeros = new int[n];
-        for(int i = 1; i < n; i++){
-            if(arr[i-1] == 0){
-                countZeros[i] = countZeros[i-1] + 1;
-            }
-            else {
-                countZeros[i] = countZeros[i-1];
-            }
-        }
-        for(int i = n-1; i >= 0; i--){
-            int shift = countZeros[i];
-            if(shift == 0) continue;
-            int copy = arr[i];
-            arr[i] = 0;
-            if(i+shift >= n) continue;
-            arr[i+shift] = copy;
+    public void duplicateZeros(int[] a) {
+        int i = 0, sh = 0;
+        for (i = 0; sh + i < a.length; ++i) sh += a[i] == 0 ? 1 : 0;
+        for (i = i - 1; sh > 0; --i) {
+            if (i + sh < a.length) a[i + sh] = a[i];
+            if (a[i] == 0) a[i + --sh] = a[i];
         }
     }
 }
-// [1,0,2,3,0,4,5,0]
-// [0,0,1,1,1,2,2,2]
-// [1,0,0,2,3,0,0,4]
