1df65e7169cc4a5dfef0ffd992160630cc311bce
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-08-30 06:57:05 +0530

    Time: 1 ms, Memory: 41.2 MB - LeetHub

---
+++ height-checker/height-checker.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        final int MAX_HEIGHT = 100;
+        int[] freq = new int[MAX_HEIGHT+1];
+        int count = 0;
+        int currHeight = 1;
+        
+        for(int height : heights) freq[height]++;
+        
+        for(int height : heights){
+            while(freq[currHeight] == 0) currHeight++;            
+            if(currHeight != height) count++;
+            freq[currHeight]--;
+        }
+        return count;
+    }
+}
