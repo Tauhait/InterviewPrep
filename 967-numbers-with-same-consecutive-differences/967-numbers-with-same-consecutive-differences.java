ef86281e0bf3066759f24e610c76f7e91931e134
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-09-03 22:01:39 +0530

    Time: 8 ms (28.13%), Space: 44 MB (10.00%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -1,31 +1,33 @@
-class Solution {
-    private List<String> numbers;
-    
-    private void backtrack(StringBuilder number, int n, int k){
-        if(number.length() == n) {
-            numbers.add(number.toString());
+ class Solution {
+
+    public int[] numsSameConsecDiff(int N, int K) {
+        if (N == 1)
+            return new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        List<Integer> results = new ArrayList<Integer>();
+        for (int num = 1; num < 10; ++num)
+            this.DFS(N - 1, num, K, results);
+
+        // convert the ArrayList to int[]
+        return results.stream().mapToInt(i->i).toArray();
+    }
+
+    protected void DFS(int N, int num, int K, List<Integer> results) {
+        if (N == 0) {
+            results.add(num);
             return;
         }
-        for(int i = 0; i <= 9; i++){
-            int nlen = number.length();
-            if(i == 0 && nlen == 0) continue;
-            if(nlen == 0) number.append(i);
-            else {
-                int absDiff = Math.abs(i - (Integer.valueOf(number.charAt(nlen-1))-48));
-                if(absDiff != k) continue;
-                number.append(i); 
+        List<Integer> nextDigits = new ArrayList<>();
+
+        Integer tailDigit = num % 10;
+        nextDigits.add(tailDigit + K);
+        if (K != 0)
+            nextDigits.add(tailDigit - K);
+        for (Integer nextDigit : nextDigits) {
+            if (0 <= nextDigit && nextDigit < 10) {
+                Integer newNum = num * 10 + nextDigit;
+                this.DFS(N - 1, newNum, K, results);
             }
-            backtrack(number, n, k);
-            number.deleteCharAt(nlen);
         }
-    }
-    public int[] numsSameConsecDiff(int n, int k) {
-        numbers = new ArrayList<String>();
-        backtrack(new StringBuilder(), n, k);
-        int[] ans = new int[numbers.size()];
-        
-        for(int i = 0; i < numbers.size(); i++)
-            ans[i] = Integer.valueOf(numbers.get(i));
-        return ans;
     }
 }
