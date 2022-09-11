e9367d8c9bd0ca186f0398c866f843660d0d0e3a
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-09-12 00:55:27 +0530

    Time: 139 ms (9.33%), Space: 68.8 MB (54.67%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
@@ -1,13 +1,26 @@
 class Solution {
+    /**
+    For each candidate, we treat him/her as the one who has the minimum efficiency in a team.
+    Then, we select the rest of the team members based on this condition.
+    
+    First of all, let's sort the candidates by efficiency in descending order.
+    Then, we will iterate through the sorted candidates.
+    At each iteration, our goal is to construct a team with at most k members, 
+    while treating the current candidate as the one with the lowest efficiency on the team.
+    We use a priority queue to store the speeds for the rest k-1 team members. 
+    The priority queue is maintained as a sliding window along with our iteration. 
+    For example, we pop out the member with the lowest speed 
+    when we exceed the predefined capacity of the queue, which is k-1.
+    */
     public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
-        int modulo = (int) Math.pow(10, 9) + 7;
+        int modulo = 1_000_000_007;
         // build tuples of (efficiency, speed)
         List<Pair<Integer, Integer>> candidates = new ArrayList<>();
         for (int i = 0; i < n; ++i) {
             candidates.add(new Pair(efficiency[i], speed[i]));
         }
         // sort the members by their efficiencies
-        Collections.sort(candidates, Comparator.comparing(o -> -o.getKey()));
+        Collections.sort(candidates, (a,b)->(b.getKey()-a.getKey()));
 
         // create a heap to keep the top (k-1) speeds
         PriorityQueue<Integer> speedHeap = new PriorityQueue<>((o1, o2) -> o1 - o2);
