2e44d2b676c4ecb0cf4a7fadd2404e6500feec43
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-09-13 10:23:48 +0530

    Time: 18 ms (18.91%), Space: 53 MB (14.25%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.java
+++ 393-utf-8-validation/393-utf-8-validation.java
@@ -1,15 +1,20 @@
 class Solution {
-    private int mask1Byte  = 0;
-    private int mask2Byte  = 192;
-    private int mask3Byte  = 224;
-    private int mask4Byte  = 240;
-    private int maskCommon = 128;
+    private final int zero = 0;
+    private final int oneNineTwo  = 192;
+    private final int twoTwoFour  = 224;
+    private final int twoFourZero = 240;
+    private final int oneTwoEight = 128;
+    private final int mask1Byte  = zero;
+    private final int mask2Byte  = oneNineTwo;
+    private final int mask3Byte  = twoTwoFour;
+    private final int mask4Byte  = twoFourZero;
+    private final int maskCommon = oneTwoEight;
     
     private boolean isValid(int start, int end, int slen, int[] data){
         if(end >= slen) return false;
         for(int i = start; i <= end && i < slen; i++){
             String temp = Integer.toString(data[i],2);
-            if((data[i] & maskCommon) != 128 || temp.charAt(1) != '0') return false;
+            if((data[i] & maskCommon) != oneTwoEight || temp.charAt(1) != '0') return false;
         }
         return true;
     }
@@ -22,13 +27,13 @@ class Solution {
             String dstr = Integer.toString(d,2);
             if(dstr.length() < 8 || dstr.charAt(0) == '0'){
                 index++;
-            }else if((d & mask4Byte) == 240 && dstr.charAt(4) == '0'){
+            }else if((d & mask4Byte) == twoFourZero && dstr.charAt(4) == '0'){
                 if(!isValid(index+1, index+3, len, data)) return false;
                 index += 4;
-            }else if((d & mask3Byte) == 224 && dstr.charAt(3) == '0'){
+            }else if((d & mask3Byte) == twoTwoFour && dstr.charAt(3) == '0'){
                 if(!isValid(index+1, index+2, len, data)) return false;
                 index += 3;
-            }else if((d & mask2Byte) == 192 && dstr.charAt(2) == '0'){
+            }else if((d & mask2Byte) == oneNineTwo && dstr.charAt(2) == '0'){
                 if(!isValid(index+1, index+1, len, data)) return false;
                 index += 2;
             }else {
