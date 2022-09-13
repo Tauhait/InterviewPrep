0036de99faf2c1bb68b9b44d38bf4d87ac64340f
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-09-13 10:18:00 +0530

    Time: 19 ms (17.88%), Space: 53.4 MB (13.21%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.java
+++ 393-utf-8-validation/393-utf-8-validation.java
@@ -5,6 +5,14 @@ class Solution {
     private int mask4Byte  = 240;
     private int maskCommon = 128;
     
+    private boolean isValid(int start, int end, int slen, int[] data){
+        if(end >= slen) return false;
+        for(int i = start; i <= end && i < slen; i++){
+            String temp = Integer.toString(data[i],2);
+            if((data[i] & maskCommon) != 128 || temp.charAt(1) != '0') return false;
+        }
+        return true;
+    }
     public boolean validUtf8(int[] data) {
         int len = data.length;
         int index = 0;
@@ -15,25 +23,13 @@ class Solution {
             if(dstr.length() < 8 || dstr.charAt(0) == '0'){
                 index++;
             }else if((d & mask4Byte) == 240 && dstr.charAt(4) == '0'){
-                if(index+3 >= len) return false;
-                for(int i = index + 1; i <= index + 3 && i < len; i++){
-                    String temp = Integer.toString(data[i],2);
-                    if((data[i] & maskCommon) != 128 || temp.charAt(1) != '0') return false;
-                }
+                if(!isValid(index+1, index+3, len, data)) return false;
                 index += 4;
             }else if((d & mask3Byte) == 224 && dstr.charAt(3) == '0'){
-                if(index+2 >= len) return false;
-                for(int i = index + 1; i <= index + 2 && i < len; i++){
-                    String temp = Integer.toString(data[i],2);
-                    if((data[i] & maskCommon) != 128 || temp.charAt(1) != '0') return false;
-                }
+                if(!isValid(index+1, index+2, len, data)) return false;
                 index += 3;
             }else if((d & mask2Byte) == 192 && dstr.charAt(2) == '0'){
-                if(index+1 >= len) return false;
-                for(int i = index + 1; i <= index + 1 && i < len; i++){
-                    String temp = Integer.toString(data[i],2);
-                    if((data[i] & maskCommon) != 128 || temp.charAt(1) != '0') return false;
-                }
+                if(!isValid(index+1, index+1, len, data)) return false;
                 index += 2;
             }else {
                 return false;
