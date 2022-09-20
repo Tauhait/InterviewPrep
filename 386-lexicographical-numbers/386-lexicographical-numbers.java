e22e6b1cb96001731938519d2d6d06d329608d24
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-09-21 01:44:39 +0530

    Time: 37 ms (16.26%), Space: 61.5 MB (10.29%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.java
+++ 386-lexicographical-numbers/386-lexicographical-numbers.java
@@ -1,17 +1,68 @@
-class Solution {    
+class Solution {
+    Node root = new Node();
     public List<Integer> lexicalOrder(int n) {
+        insert(n);
         List<Integer> res = new ArrayList<>();
-        for(int i = 1; i < 10; ++i) dfs(i, n, res);
+        add(root, res, 0, n);
         return res;
-    }    
-    private void dfs(int cur, int n, List<Integer> res){
-        if(cur > n) return;
-        else {
-            res.add(cur);
-            for(int i = 0; i < 10; ++i){
-                if(10*cur+i > n) return;
-                dfs(10*cur+i, n, res);
+    }
+    
+    public void insert(int n) {
+        for (int i = 1; i <= n; i++) {
+            Node node = root;
+            String s = "" + i;
+            for (char ch : s.toCharArray()){
+                int key = ch - '0';
+                if (!node.containsKey(key)) 
+                    node.put(key, new Node());
+                node = node.get(key);
+            }
+            node.setEnd();
+        }
+    }
+    
+    public void add(Node node, List<Integer> res, int ans, int n) {
+        if (ans > n) 
+            return;
+        for (int i = 0; i < 10; i++) {
+            if (ans * 10 + i > n) return;
+            if (node.containsKey(i)) {
+                Node temp = node;
+                res.add(ans * 10 + i);
+                temp = temp.get(i);
+                add(temp, res, ans * 10 + i, n);
             }
         }
+    }
+    
+}
+
+class Node {
+    Node[] node;
+    boolean flag;
+    
+    Node() {
+        node = new Node[10];
+        flag = false;
+    }
+    
+    boolean containsKey(int n) {
+        return node[n] != null;
+    }
+    
+    Node get(int n) {
+        return node[n];
+    }
+    
+    void put(int n, Node newNode) {
+        node[n] = newNode;
+    }
+    
+    boolean isEnd() {
+        return flag;
+    }
+    
+    void setEnd() {
+        flag = true;
     }
 }
