944241c71c38004d110eebd60a88de423f9000e0
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-09-21 20:44:35 +0530

    Time: 2 ms (89.16%), Space: 232.8 MB (57.23%) - LeetHub

--- 431-encode-n-ary-tree-to-binary-tree/431-encode-n-ary-tree-to-binary-tree.java
+++ 431-encode-n-ary-tree-to-binary-tree/431-encode-n-ary-tree-to-binary-tree.java
@@ -16,26 +16,15 @@ class Node {
     }
 };
 */
-
-/*
-// Definition for a binary tree node.
-public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-   TreeNode(int x) { val = x; }
-}
-*/
-
-class Pair<U, V> {
-  public U first;
-  public V second;
-
-  public Pair(U first, V second) {
-    this.first = first;
-    this.second = second;
-  }
-}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 
 class Codec {
 
@@ -44,35 +33,20 @@ class Codec {
     if (root == null) {
       return null;
     }
+
     TreeNode newRoot = new TreeNode(root.val);
-    Pair<TreeNode, Node> head = new Pair<TreeNode, Node>(newRoot, root);
 
-    // Add the first element to kickoff the loop
-    Queue<Pair<TreeNode, Node>> queue = new ArrayDeque<Pair<TreeNode, Node>>();
-    queue.add(head);
+    // Encode the first child of n-ary node to the left node of binary tree.
+    if (root.children.size() > 0) {
+      Node firstChild = root.children.get(0);
+      newRoot.left = this.encode(firstChild);
+    }
 
-    while (queue.size() > 0) {
-      Pair<TreeNode, Node> pair = queue.remove();
-      TreeNode bNode = pair.first;
-      Node nNode = pair.second;
-
-      // Encoding the children nodes into a list of TreeNode.
-      TreeNode prevBNode = null, headBNode = null;
-      for (Node nChild : nNode.children) {
-        TreeNode newBNode = new TreeNode(nChild.val);
-        if (prevBNode == null) {
-          headBNode = newBNode;
-        } else {
-          prevBNode.right = newBNode;
-        }
-        prevBNode = newBNode;
-
-        Pair<TreeNode, Node> nextEntry = new Pair<TreeNode, Node>(newBNode, nChild);
-        queue.add(nextEntry);
-      }
-
-      // Attach the list of children to the left node.
-      bNode.left = headBNode;
+    // Encoding the rest of the sibling nodes.
+    TreeNode sibling = newRoot.left;
+    for (int i = 1; i < root.children.size(); ++i) {
+      sibling.right = this.encode(root.children.get(i));
+      sibling = sibling.right;
     }
 
     return newRoot;
@@ -83,31 +57,14 @@ class Codec {
     if (root == null) {
       return null;
     }
+
     Node newRoot = new Node(root.val, new ArrayList<Node>());
 
-    // adding the first element to kickoff the loop
-    Queue<Pair<Node, TreeNode>> queue = new ArrayDeque<Pair<Node, TreeNode>>();
-    Pair<Node, TreeNode> head = new Pair<Node, TreeNode>(newRoot, root);
-    queue.add(head);
-
-    while (queue.size() > 0) {
-      Pair<Node, TreeNode> entry = queue.remove();
-      Node nNode = entry.first;
-      TreeNode bNode = entry.second;
-
-      // Decoding the children list
-      TreeNode firstChild = bNode.left;
-      TreeNode sibling = firstChild;
-      while (sibling != null) {
-        Node nChild = new Node(sibling.val, new ArrayList<Node>());
-        nNode.children.add(nChild);
-
-        // prepare the decoding the children of the child, by standing in the queue.
-        Pair<Node, TreeNode> nextEntry = new Pair<Node, TreeNode>(nChild, sibling);
-        queue.add(nextEntry);
-
-        sibling = sibling.right;
-      }
+    // Decoding all the children nodes
+    TreeNode sibling = root.left;
+    while (sibling != null) {
+      newRoot.children.add(this.decode(sibling));
+      sibling = sibling.right;
     }
 
     return newRoot;
