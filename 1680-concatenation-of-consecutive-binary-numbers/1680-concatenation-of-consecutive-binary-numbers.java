b8650b894809775f8e3fa68dd46d9efe3a065095
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-09-23 19:59:11 +0530

    Time: 115 ms (70.13%), Space: 40.8 MB (81.82%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
@@ -3,10 +3,12 @@ class Solution {
         final int MOD = 1_000_000_007;
         long result = 0, len = 0;
         for(int i = 1; i <= n; i++){
-            if(Math.pow(2, (int)(Math.log10(i)/Math.log10(2))) == i)
+            // if(Math.pow(2, (int)(Math.log10(i)/Math.log10(2))) == i)
+            //     ++len;
+            //With bitwise operation, we can check whether a number is the power of 2 in O(1).
+            if((i & (i-1)) == 0)
                 ++len;
-            result = (result << len) % MOD;
-            result = (result + i)    % MOD;
+            result = ((result << len) | i) % MOD;
         }
         return (int)result;
     }
