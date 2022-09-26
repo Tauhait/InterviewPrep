189886931e9b43aba8dd0b88d5d5ee945ce286f7
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-09-26 10:21:46 +0530

    Time: 2 ms (69.35%), Space: 42.6 MB (43.09%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
@@ -0,0 +1,64 @@
+class Solution {    
+    public boolean equationsPossible(String[] equations) {
+        UnionFind uf = new UnionFind(26);
+        for(String equation : equations){
+            int var1 = equation.charAt(0)-'a';
+            int var2 = equation.charAt(3)-'a';
+            if(equation.charAt(1) == '=')
+                uf.union(var1, var2);
+        }
+        for(String equation : equations){
+            int var1 = equation.charAt(0)-'a';
+            int var2 = equation.charAt(3)-'a';
+            if(equation.charAt(1) == '!'){
+                if(uf.find(var1) == uf.find(var2))
+                    return false;
+            }else {
+                if(uf.find(var1) != uf.find(var2))
+                    return false;
+            }                
+        }
+        return true;
+    }
+}
+class UnionFind {
+    private int[] group;
+    private int[] rank;
+    
+    public int[] getGroup(){
+        return group;
+    }
+    public int[] getRank(){
+        return rank;
+    }
+    public UnionFind(int size){
+        group = new int[size];
+        rank = new int[size];
+        for(int i = 0; i < size; i++){
+            group[i] = i;
+            rank[i] = 0;
+        }
+    }
+    
+    public void union(int x, int y){
+        int groupX = find(x);
+        int groupY = find(y);
+        if(groupX != groupY){
+            if(rank[groupX] > rank[groupY]){
+                group[groupY] = groupX;
+            }else if(rank[groupX] < rank[groupY]){
+                group[groupX] = groupY;
+            }else {
+                group[groupX] = groupY;
+                rank[groupX]++;
+            }
+        }
+    }
+    
+    public int find(int node){
+        if(node == group[node]){
+            return node;
+        }
+        return group[node] = find(group[node]);
+    }
+}
