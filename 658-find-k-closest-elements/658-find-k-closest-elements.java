9c759ff1d58af7c9568127837f7a756d8d518a86
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-09-29 14:42:41 +0530

    Time: 3 ms (99.87%), Space: 43.8 MB (98.33%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,25 +1,18 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        // Initialize binary search bounds
-        int left = 0;
-        int right = arr.length - k;
-        
-        // Binary search against the criteria described
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (x - arr[mid] > arr[mid + k] - x) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        
-        // Create output in correct format
-        List<Integer> result = new ArrayList<Integer>();
-        for (int i = left; i < left + k; i++) {
-            result.add(arr[i]);
-        }
-        
-        return result;
+        int lo = 0;
+		int hi = arr.length - 1;
+		while (hi - lo >= k) {
+			if (Math.abs(arr[lo] - x) > Math.abs(arr[hi] - x)) {
+				lo++;
+			} else {
+				hi--;
+			}
+		}
+		List<Integer> result = new ArrayList<>(k);
+		for (int i = lo; i <= hi; i++) {
+			result.add(arr[i]);
+		}
+		return result;
     }
 }
