4764252c17bcaa76de9c1a35e7d448ae84fddee7
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-10-11 11:13:28 +0530

    Time: 33 ms (5.02%), Space: 97.6 MB (5.02%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,15 +1,31 @@
 class Solution {
-    public boolean increasingTriplet(int[] nums) {
-        int smallest = Integer.MAX_VALUE;
-        int smaller = Integer.MAX_VALUE;
-        for(int num : nums){
-            if(num <= smallest)
-                smallest = num;
-            else if(num <= smaller)
-                smaller = num;
+    private int findUpperBoundBinarySearch(long[] dp, int key){
+        int low = 0, high = dp.length-1;
+        while(low < high){
+            int mid = (low+high)>>>1;
+            if(dp[mid] < key)
+                low = mid + 1;
             else 
-                return true;
+                high = mid;
         }
-        return false;
+        return low;
+    }
+    public boolean increasingTriplet(int[] nums) {
+        int n = nums.length;
+        long[] dp = new long[n+1];
+        for(int i = 1; i <= n; i++)
+            dp[i] = Long.MAX_VALUE;
+        dp[0] = Long.MIN_VALUE;
+        for(int i = 0; i < n; i++){
+            int j = findUpperBoundBinarySearch(dp, nums[i]);
+            if (j > 0 && dp[j-1] < nums[i] && nums[i] < dp[j])
+                dp[j] = nums[i];
+        }
+        int ans = 0;
+        for (int i = 0; i <= n; i++) {
+            if (dp[i] < Long.MAX_VALUE)
+                ans = i;
+        }
+        return ans >= 3;
     }
 }
