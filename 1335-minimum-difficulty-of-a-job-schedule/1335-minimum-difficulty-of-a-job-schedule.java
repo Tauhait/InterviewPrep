73636c3b747e40d178f6a0afed0f9e4304f855f2
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-10-16 13:52:13 +0530

    Time: 49 ms (20.77%), Space: 42.4 MB (26.07%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
@@ -1,42 +1,33 @@
 class Solution {
     private int[] jobDifficulty;
-    private int n;
-    private int d;
-    private int[] hardestJobRemaining;
-    private int[][] memo;
-    public int minDifficulty(int[] jobDifficulty, int d) {
-        n = jobDifficulty.length;
-        this.d = d;
-        this.jobDifficulty = jobDifficulty;
-        if(d > n){
-            return -1;
-        }
-        hardestJobRemaining = new int[n];
-        int hardest = 0;
-        for(int i = n - 1; i >= 0; i--){
-            hardest = Math.max(hardest, jobDifficulty[i]);
-            hardestJobRemaining[i] = hardest;
-        }
-        memo = new int[n][d + 1];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(memo[i], -1);
-        }
-        return dp(0, 1);        
-    }
-    private int dp(int i, int day){
-        if(day == d){
-            return hardestJobRemaining[i];
-        }
-        if(memo[i][day] == -1){            
-            int jobPossibilities = n - (d - day);
-            int maxDifficulty = 0;
-            int best = Integer.MAX_VALUE;
-            for(int j = i; j < jobPossibilities; j++){
-                maxDifficulty = Math.max(maxDifficulty, jobDifficulty[j]);
-                best = Math.min(best, maxDifficulty + dp(j + 1, day + 1));             
+    private Integer[][] memo;
+    
+    private int dp(int idx, int daysRemain){
+        if(memo[idx][daysRemain] != null)
+            return memo[idx][daysRemain];
+        if(daysRemain == 1){
+            int difficulty = 0;
+            for(int i = idx; i < jobDifficulty.length; i++){
+                difficulty = Math.max(difficulty, jobDifficulty[i]);
             }
-            memo[i][day] = best;
+            return difficulty;
         }
-        return memo[i][day];
+        int minJobDifficulty = Integer.MAX_VALUE;
+        int dailyMaxJobDifficulty = 0;
+        for(int i = idx; i < jobDifficulty.length-daysRemain+1; i++){
+            dailyMaxJobDifficulty = Math.max(dailyMaxJobDifficulty, jobDifficulty[i]);
+            minJobDifficulty = Math.min(minJobDifficulty, 
+                                        dailyMaxJobDifficulty + dp(i+1, daysRemain-1));
+        }
+        return memo[idx][daysRemain] = minJobDifficulty;
+    }
+    
+    public int minDifficulty(int[] jobDifficulty, int d) {
+        int n = jobDifficulty.length;
+        if(n < d)
+            return -1;
+        memo = new Integer[n][d+1];
+        this.jobDifficulty = jobDifficulty;
+        return dp(0, d);
     }
 }
