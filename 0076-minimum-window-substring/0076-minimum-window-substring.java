c24da425065d2875d70aa52e62d78d0861b7f222
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-10-23 01:37:47 +0530

    Time: 7 ms (87.61%), Space: 44.2 MB (66.03%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,37 +1,30 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int windowStart = 0, matched = 0, minLength = s.length() + 1, subStrStart = 0;
-        Map<Character, Integer> charFrequencyMap = new HashMap<>();
-        for (char chr : t.toCharArray())
-          charFrequencyMap.put(chr, charFrequencyMap.getOrDefault(chr, 0) + 1);
-
-        // try to extend the range [windowStart, windowEnd]
-        for (int windowEnd = 0; windowEnd < s.length(); windowEnd++) {
-          char rightChar = s.charAt(windowEnd);
-          if (charFrequencyMap.containsKey(rightChar)) {
-            charFrequencyMap.put(rightChar, charFrequencyMap.get(rightChar) - 1);
-            if (charFrequencyMap.get(rightChar) >= 0) // count every matching of a character
-              matched++;
-          }
-
-          // shrink the window if we can, finish as soon as we remove a matched character
-          while (matched == t.length()) {
-            if (minLength > windowEnd - windowStart + 1) {
-              minLength = windowEnd - windowStart + 1;
-              subStrStart = windowStart;
+        Integer[] tFreq = new Integer[128];
+        for(char c : t.toCharArray())
+            tFreq[c] = tFreq[c] == null ? 1 : tFreq[c]+1;
+        
+        int start, end, minSize = s.length()+1, minWinStart = 0, matched = 0;
+        for(start = 0, end = 0; end < s.length(); end++){
+            char c = s.charAt(end);
+            if(tFreq[c] != null){
+                tFreq[c]--;
+                if(tFreq[c] >= 0)
+                    matched++;
             }
-
-            char leftChar = s.charAt(windowStart++);
-            if (charFrequencyMap.containsKey(leftChar)) {
-              // note that we could have redundant matching characters, therefore we'll decrement the
-              // matched count only when a useful occurrence of a matched character is going out of the window
-              if (charFrequencyMap.get(leftChar) == 0)
-                matched--;
-              charFrequencyMap.put(leftChar, charFrequencyMap.get(leftChar) + 1);
+            while(matched == t.length()){
+                if(end-start+1 < minSize){ 
+                    minSize = end-start+1;
+                    minWinStart = start;
+                }
+                char left = s.charAt(start++);
+                if(tFreq[left] != null){
+                    if(tFreq[left] == 0)
+                        matched--;
+                    tFreq[left]++;
+                }                
             }
-          }
         }
-
-        return minLength > s.length() ? "" : s.substring(subStrStart, subStrStart + minLength);
+        return minSize > s.length() ? "" : s.substring(minWinStart, minWinStart+minSize);
     }
 }
