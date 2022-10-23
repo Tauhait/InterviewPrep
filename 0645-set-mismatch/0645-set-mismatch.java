725f1a36415155c59733ec640ee5a2335e0e7b9b
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-10-23 12:33:44 +0530

    Time: 5 ms (60.11%), Space: 54.9 MB (30.32%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,27 +1,16 @@
 public class Solution {
     public int[] findErrorNums(int[] nums) {
-        int xor = 0, xor0 = 0, xor1 = 0;
-        for (int n: nums)
-            xor ^= n;
-        for (int i = 1; i <= nums.length; i++)
-            xor ^= i;
-        int rightmostbit = xor & ~(xor - 1);
+        int dup = -1, missing = 1;
         for (int n: nums) {
-            if ((n & rightmostbit) != 0)
-                xor1 ^= n;
+            if (nums[Math.abs(n) - 1] < 0)
+                dup = Math.abs(n);
             else
-                xor0 ^= n;
+                nums[Math.abs(n) - 1] *= -1;
         }
-        for (int i = 1; i <= nums.length; i++) {
-            if ((i & rightmostbit) != 0)
-                xor1 ^= i;
-            else
-                xor0 ^= i;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > 0)
+                missing = i + 1;
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == xor0)
-                return new int[]{xor0, xor1};
-        }
-        return new int[]{xor1, xor0};
+        return new int[]{dup, missing};
     }
 }
