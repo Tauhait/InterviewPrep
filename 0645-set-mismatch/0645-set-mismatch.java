7481781112d26d109abaf9ac19eda5207e53a7eb
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-10-23 12:29:30 +0530

    Time: 16 ms (26.77%), Space: 54.3 MB (72.93%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,18 +1,26 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        Set<Integer> numSet = new HashSet<Integer>();
-        int[] ans = new int[2];
-        int n = nums.length;
-        int sum = n*(n+1)/2;
-        int currSum = 0;
-        for(int num : nums){
-            if(numSet.contains(num))
-                ans[0] = num;
-            else
-                currSum += num;
-            numSet.add(num);
-        }
-        ans[1] = sum-currSum;
-        return ans;
+        /** Assume m is the missing and d is the duplicate element
+         diff= m-d;
+         squareDiff= m^2-d^2;
+         sum=m+d= squareDiff/diff
+         =>sum    = (m+d)(m-d)/(m-d);
+         now m=(sum+diff)2;
+         and d= (sum-diff)2;
+        **/
+        int diff=0;
+        int sqaureDiff=0;
+        for(int i=0;i<nums.length;i++){
+            /** 
+            The order doesnot matter. keep adding the 1 to n and 
+            simultaneously subracting  corresponding array element.
+            Use i+1 to get 1 to n  since i is the index number which is zero based.
+            **/
+            diff+=(i+1)-nums[i];
+            /** squareDiff is also calculated in the same way as diff is calculated. **/
+            sqaureDiff+= (i+1)*(i+1)-nums[i]*nums[i];
+        } 
+       int sum=sqaureDiff/diff;
+      return new int[]{(sum-diff)/2,(sum+diff)/2};
     }
 }
