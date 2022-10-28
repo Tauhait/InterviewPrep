a4c9aeaf1ecbc05815f59dc8cad392fd2b6e3109
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-10-28 09:23:38 +0530

    Time: 10 ms (88.14%), Space: 55.2 MB (79.06%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs == null || strs.length == 0) return Collections.emptyList();
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s: strs){
+            char[] frequencyArr = new char[26];
+            for(int i = 0;i<s.length();i++){
+                frequencyArr[s.charAt(i) - 'a']++;
+            }
+            String key = new String(frequencyArr);
+            List<String> tempList = map.getOrDefault(key, new LinkedList<String>());
+            tempList.add(s);
+            map.put(key,tempList);
+        }
+        return new LinkedList<>(map.values());
+    }
+}
