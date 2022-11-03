c8c43503c4a84f8cf1c733cd09d907aed0bc7381
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-11-03 19:28:51 +0530

    Time: 390 ms (15.71%), Space: 122.9 MB (38.27%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,49 +1,43 @@
-class Solution {    
+class Solution {
     public int longestPalindrome(String[] words) {
-        Map<String,Integer> dict = new HashMap<>();
-        Map<String,Integer> map = new HashMap<>();
+        if (words == null || words.length == 0) {
+            return 0;
+        }
         
-        int len = 0;
-        for(String word : words){
-            char first = word.charAt(0);
-            char last  = word.charAt(1);
+        int result = 0;
+        HashMap<String, Integer> map = new HashMap<>();
+        int palWordCount = 0;
+        
+        for (String word: words) {
+            String reverseWord = "" + word.charAt(1) + word.charAt(0);
             
-            if(first == last){
-                map.put(word, map.getOrDefault(word,0)+1);
-            }else {
-                dict.put(word, dict.getOrDefault(word,0)+1);
-                String rev = (new StringBuilder(word)).reverse().toString();
-                if(dict.containsKey(rev)){
-                    len += 4;
-                    dict.put(rev,dict.get(rev)-1);
-                    dict.put(word,dict.get(word)-1);
-                    if(dict.get(rev) == 0){ 
-                        dict.remove(rev);
-                    }
-                    if(dict.get(word) == 0){
-                        dict.remove(word);
-                    }
-                    
-                    // System.out.println(word+","+rev);
+            if (map.containsKey(reverseWord)) {
+                result += 4;
+                
+                int value = map.get(reverseWord);
+                if (value == 1) {
+                    map.remove(reverseWord);
+                } else {
+                    map.put(reverseWord, value - 1);
+                }
+                
+                if (word.equals(reverseWord)) {
+                    palWordCount--;
+                }
+            } else {
+                if (map.containsKey(word)) {
+                    int value = map.get(word);
+                    map.put(word, value + 1);
+                } else {
+                    map.put(word, 1);
+                }
+                
+                if (word.equals(reverseWord)) {
+                    palWordCount++;
                 }
             }
         }
-        boolean isMidTaken = false;
-        if(map.size() > 0){   
-            for(Map.Entry<String,Integer> entry : map.entrySet()){
-                String k  = entry.getKey();
-                Integer v = entry.getValue();
-                
-                if((v&1) == 1){
-                    if(isMidTaken)
-                        v -= 1;
-                    else 
-                        isMidTaken = true;
-                }
-                // System.out.println(k);
-                len += (v<<1);
-            }
-        }        
-        return len;
+        
+        return palWordCount > 0 ? result + 2 : result;
     }
 }
