b69351419f5172d3ff45af8ee4f9ea9b5a28506f
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-11-03 19:30:35 +0530

    Time: 24 ms (90.69%), Space: 96.9 MB (73.89%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,43 +1,21 @@
 class Solution {
     public int longestPalindrome(String[] words) {
-        if (words == null || words.length == 0) {
-            return 0;
+        int counter[][] = new int[26][26];
+        int ans = 0;
+        for (String w: words) {
+            int a = w.charAt(0) - 'a', b = w.charAt(1) - 'a';
+            if (counter[b][a] > 0) {
+                ans += 4; 
+                counter[b][a]--; 
+            }
+            else counter[a][b]++;
         }
-        
-        int result = 0;
-        HashMap<String, Integer> map = new HashMap<>();
-        int palWordCount = 0;
-        
-        for (String word: words) {
-            String reverseWord = "" + word.charAt(1) + word.charAt(0);
-            
-            if (map.containsKey(reverseWord)) {
-                result += 4;
-                
-                int value = map.get(reverseWord);
-                if (value == 1) {
-                    map.remove(reverseWord);
-                } else {
-                    map.put(reverseWord, value - 1);
-                }
-                
-                if (word.equals(reverseWord)) {
-                    palWordCount--;
-                }
-            } else {
-                if (map.containsKey(word)) {
-                    int value = map.get(word);
-                    map.put(word, value + 1);
-                } else {
-                    map.put(word, 1);
-                }
-                
-                if (word.equals(reverseWord)) {
-                    palWordCount++;
-                }
+        for (int i = 0; i < 26; i++) {
+            if (counter[i][i] > 0) {
+                ans += 2;
+                break;
             }
         }
-        
-        return palWordCount > 0 ? result + 2 : result;
+        return ans;
     }
 }
