211172135d69dfbf869902cbfd54afa191c43d2d
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-11-07 19:28:40 +0530

    Time: 20 ms, Memory: 55.7 MB - LeetHub

--- gray-code/gray-code.java
+++ gray-code/gray-code.java
@@ -1,23 +1,8 @@
 class Solution {
     public List<Integer> grayCode(int n) {
-        int loop = (1<<n);
-        List<Integer> ans = new ArrayList<>();
-        for(int i = 0; i < loop; i++){
-            int numCopy = i;
-            int lastbit = (numCopy&1);
-            int gray = 0;
-            int shift = 0;
-            numCopy >>>= 1;
-            while(numCopy != 0){
-                int bit = (numCopy&1);
-                gray |= ((bit ^ lastbit) << shift);
-                numCopy >>>= 1;
-                shift++;
-                lastbit = bit;
-            }
-            gray |= (lastbit << shift);
-            ans.add(gray);
-        }
-        return ans;
+        List<Integer> result = new LinkedList<>();
+        for (int i = 0; i < (1<<n); i++) 
+            result.add(i ^ (i>>1));
+        return result;
     }
 }
