359814872ef64c488afcebb2bfc73d7e0e5cb536
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-11-10 10:56:17 +0530

    Time: 4 ms (62.48%), Space: 48.1 MB (47.23%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,17 @@
+public class Solution {
+    private int popCount(int x) {
+        int count;
+        for (count = 0; x != 0; ++count) {
+            x &= x - 1; // zeroing out the least significant nonzero bit
+        }
+        return count;
+    }
+
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        for (int x = 0; x <= n; ++x) {
+            ans[x] = popCount(x);
+        }
+        return ans;
+    }
+}
