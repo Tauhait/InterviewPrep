1985169ed239792473c5ee81be0701b65640a281
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-11-11 00:42:35 +0530

    Time: 6 ms (52.81%), Space: 45.4 MB (35.99%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        // difference between two numbers (x and y) which were seen only once
+        int bitmask = 0;
+        for (int num : nums) 
+            bitmask ^= num;
+
+        // rightmost 1-bit diff between x and y
+        int rMask = bitmask & (-bitmask);
+
+        int x = 0;
+        // bitmask which will contain only x
+        for (int num : nums) 
+            if ((num & rMask) != 0) 
+                x ^= num;
+
+        return new int[]{x, bitmask^x};
+    }
+}
