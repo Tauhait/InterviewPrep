72fb0091f35e280b416ae968f1eada603bc59822
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-11-12 13:39:47 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (91.75%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -1,58 +1,50 @@
 /*
 // Definition for a Node.
 class Node {
-    int val;
-    Node next;
-    Node random;
+    public int val;
+    public Node next;
+    public Node random;
 
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
+    public Node() {}
+
+    public Node(int _val,Node _next,Node _random) {
+        val = _val;
+        next = _next;
+        random = _random;
     }
-}
+};
 */
+public class Solution {
+  // HashMap which holds old nodes as keys and new nodes as its values.
+  HashMap<Node, Node> visitedHash = new HashMap<Node, Node>();
 
-class Solution {
-    public Node copyRandomList(Node head) {
-        Map<Node, Integer> map = new HashMap<>();
-        Node copyHead = head;
-        Node copyListHead = null;
-        int index = 0;
-        Node iterator = null;
-        while(head != null){
-            if(copyListHead == null){
-                Node r = new Node(head.val);
-                copyListHead = r;
-                iterator = r;
-            }else {
-                Node r = new Node(head.val);
-                iterator.next = r;
-                iterator = iterator.next;
-            }
-            map.put(head, index++);
-            head = head.next;
-        }
-        
-        head = copyHead;
-        int currentPos = 0;
-        Node nextNode = copyListHead;
-        while(head != null){
-            iterator = copyListHead;
-            if(head.random == null){
-                nextNode.random = null;
-                nextNode = nextNode.next;
-                head = head.next;
-                continue;
-            }                
-            int goTo = map.get(head.random);
-            head = head.next;
-            while(goTo-- > 0){
-                iterator = iterator.next;
-            }
-            nextNode.random = iterator;
-            nextNode = nextNode.next;
-        }
-        return copyListHead;
+  public Node copyRandomList(Node head) {
+
+    if (head == null) {
+      return null;
     }
+
+    // If we have already processed the current node, then we simply return the cloned version of
+    // it.
+    if (this.visitedHash.containsKey(head)) {
+      return this.visitedHash.get(head);
+    }
+
+    // Create a new node with the value same as old node. (i.e. copy the node)
+    Node node = new Node(head.val, null, null);
+
+    // Save this value in the hash map. This is needed since there might be
+    // loops during traversal due to randomness of random pointers and this would help us avoid
+    // them.
+    this.visitedHash.put(head, node);
+
+    // Recursively copy the remaining linked list starting once from the next pointer and then from
+    // the random pointer.
+    // Thus we have two independent recursive calls.
+    // Finally we update the next and random pointers for the new node created.
+    node.next = this.copyRandomList(head.next);
+    node.random = this.copyRandomList(head.random);
+
+    return node;
+  }
 }
