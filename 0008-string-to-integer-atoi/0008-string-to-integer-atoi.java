3095db4b54a6f90b700c9ee50d1921af147967a5
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-11-12 17:11:08 +0530

    Time: 4 ms (42.91%), Space: 43.4 MB (23.05%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,22 +1,27 @@
 class Solution {
-    public int myAtoi(String s) {
-        StringBuilder sb = new StringBuilder(s.trim());
-        int len = sb.length();
+    public int myAtoi(String sb) {
+        int left = 0, right = sb.length()-1;
+        while(left+1 <= right && sb.charAt(left) == ' ')
+            left++;
+        while(right-1 >= left && sb.charAt(right) == ' ')
+            right--;
+        // int len = sb.length();
+        int len = right-left+1;
         if(len == 0)
             return 0;
         if(len == 1)
-            return Character.isDigit(s.charAt(0)) ? s.charAt(0) - '0' : 0;
+            return Character.isDigit(sb.charAt(left)) ? sb.charAt(left) - '0' : 0;
 
         
         long ans = 0;
-        int index = 0;
+        int index = left;
         int negPos = sb.charAt(index) == '-' ? -1 : 1;
         if(negPos == -1 || sb.charAt(index) == '+'){
             index++;
         }
         if(!Character.isDigit(sb.charAt(index)))
             return 0;
-        for(int i = index; i < len; i++){
+        for(int i = index; i <= right; i++){
             if(!Character.isDigit(sb.charAt(i)))
                 break;
             ans *= 10;
