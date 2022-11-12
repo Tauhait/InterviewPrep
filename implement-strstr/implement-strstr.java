c6c0e6d6cd9a1cf7fea9d78475f9208a0b4737e5
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-11-12 17:14:52 +0530

    Time: 0 ms, Memory: 40 MB - LeetHub

---
+++ implement-strstr/implement-strstr.java
@@ -0,0 +1,33 @@
+class Solution {
+    //https://www.youtube.com/watch?v=V5-7GzOfADQ
+    //https://www.scaler.com/topics/data-structures/kmp-algorithm/
+    //https://www.youtube.com/watch?v=JoF0Z7nVSrA
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) return 0;
+        int[] lps = computeKMPTable(needle);
+        int i = 0, j = 0, n = haystack.length(), m = needle.length();
+        while (i < n) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                ++i; ++j;
+                if (j == m) return i - m; // found solution
+            } else {
+                if (j != 0) j = lps[j - 1]; // try match with longest prefix suffix
+                else i++; // don't match -> go to next character of `haystack` string
+            }
+        }
+        return -1;
+    }
+    private int[] computeKMPTable(String pattern) {
+        int i = 1, j = 0, n = pattern.length();
+        int[] lps = new int[n];
+        while (i < n) {
+            if (pattern.charAt(i) == pattern.charAt(j)) {
+                lps[i++] = ++j;
+            } else {
+                if (j != 0) j = lps[j - 1]; // try match with longest prefix suffix
+                else i++; // don't match -> go to next character
+            }
+        }
+        return lps;
+    }
+}
