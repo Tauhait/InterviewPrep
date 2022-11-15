cd0c15f7b46e9381a79d73e13d463ef48e6779ed
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-11-15 21:44:10 +0530

    Time: 0 ms (100.00%), Space: 50.4 MB (13.89%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -14,33 +14,56 @@
  * }
  */
 class Solution {
-    public int countNodes(TreeNode root) {
-        if(root == null)
-            return 0;
-        int level = 0;
-        TreeNode copyRoot = root;
-        while(copyRoot != null){
-            level++;
-            copyRoot = copyRoot.right;            
-        }
-        int lastLevel = countLastLevel(root, 0, level);
-        
-        // if((lastLevel & (lastLevel-1)) == 0){
-        //     return lastLevel + (lastLevel-1);
-        // }
-        
-        int ans = 0;
-        // System.out.println(level+","+lastLevel);
-        ans = ((1<<level)-1) + lastLevel;
-        return ans;
+  // Return tree depth in O(d) time.
+  public int computeDepth(TreeNode node) {
+    int d = 0;
+    while (node.left != null) {
+      node = node.left;
+      ++d;
     }
-    private int countLastLevel(TreeNode rt, int level, int maxLevel){
-        if(rt == null)
-            return 0;
-        if(rt.left == null && rt.right == null)
-            return level == maxLevel ? 1 : 0;
-        int count = countLastLevel(rt.left,  level+1, maxLevel) + 
-                    countLastLevel(rt.right, level+1, maxLevel);
-        return count;
+    return d;
+  }
+
+  // Last level nodes are enumerated from 0 to 2**d - 1 (left -> right).
+  // Return True if last level node idx exists. 
+  // Binary search with O(d) complexity.
+  public boolean exists(int idx, int d, TreeNode node) {
+    int left = 0, right = (int)Math.pow(2, d) - 1;
+    int pivot;
+    for(int i = 0; i < d; ++i) {
+      pivot = left + (right - left) / 2;
+      if (idx <= pivot) {
+        node = node.left;
+        right = pivot;
+      }
+      else {
+        node = node.right;
+        left = pivot + 1;
+      }
     }
+    return node != null;
+  }
+
+  public int countNodes(TreeNode root) {
+    // if the tree is empty
+    if (root == null) return 0;
+
+    int d = computeDepth(root);
+    // if the tree contains 1 node
+    if (d == 0) return 1;
+
+    // Last level nodes are enumerated from 0 to 2**d - 1 (left -> right).
+    // Perform binary search to check how many nodes exist.
+    int left = 1, right = (int)Math.pow(2, d) - 1;
+    int pivot;
+    while (left <= right) {
+      pivot = left + (right - left) / 2;
+      if (exists(pivot, d, root)) left = pivot + 1;
+      else right = pivot - 1;
+    }
+
+    // The tree contains 2**d - 1 nodes on the first (d - 1) levels
+    // and left nodes on the last level.
+    return (int)Math.pow(2, d) - 1 + left;
+  }
 }
