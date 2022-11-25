62d3af2093b317a8478c70f578b0673de8f6eb4d
Tuhin Dutta <dt_tauhait@hotmail.com>
2022-11-25 12:49:59 +0530

    Time: 87 ms (68.26%), Space: 49.6 MB (96.89%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int sumSubarrayMins(int[] A) {
+        Stack<Integer> s = new Stack<>();
+        int[] dp = new int[A.length];
+        int total = 0;
+        for (int i = 0; i < A.length; i++) {
+            while (!s.isEmpty() && A[s.peek()] > A[i]) 
+                s.pop();
+            if (s.isEmpty()) 
+                dp[i] = (i + 1) * A[i];
+            else 
+                dp[i] = dp[s.peek()] + (i - s.peek()) * A[i];
+            total = (total + dp[i] ) % 1000000007;
+            s.push(i);
+        }
+        return total;
+    }
+}
