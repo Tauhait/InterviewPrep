8289bc17ec09be3cae8e590596a18c4781790e1d
Tuhin Dutta <dt_tauhait@hotmail.com>
2023-02-01 08:20:38 +0530

    Time: 2 ms, Memory: 44.7 MB - LeetHub

---
+++ flatten-nested-list-iterator/flatten-nested-list-iterator.java
@@ -0,0 +1,57 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return empty list if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+import java.util.NoSuchElementException;
+
+public class NestedIterator implements Iterator<Integer> {
+    
+    private List<Integer> integers = new ArrayList<Integer>();
+    private int position = 0; // Pointer to next integer to return.
+    
+    public NestedIterator(List<NestedInteger> nestedList) {
+        flattenList(nestedList);
+    }
+
+    // Recursively unpacks a nested list in DFS order.
+    private void flattenList(List<NestedInteger> nestedList) {
+        for (NestedInteger nestedInteger : nestedList) {
+            if (nestedInteger.isInteger()) {
+                integers.add(nestedInteger.getInteger());
+            } else {
+                flattenList(nestedInteger.getList());
+            }
+        }
+    }
+    
+    @Override
+    public Integer next() {
+        // As per Java specs, we should throw an exception if no more ints.
+        if (!hasNext()) throw new NoSuchElementException();
+        // Return int at current position, and then *after*, increment position.
+        return integers.get(position++);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position < integers.size();
+    }
+}
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
