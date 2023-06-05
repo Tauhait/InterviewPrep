22436a2632859ea84a5445f8b52087083f5a3582
Tuhin Dutta <dt_tauhait@hotmail.com>
2023-06-06 00:01:46 +0530

    Time: 15 ms (6.38%) | Memory: 60.3 MB (5.57%) - LeetSync

--- tree/practice/maximum-subarray/maximum-subarray.java
+++ tree/practice/maximum-subarray/maximum-subarray.java
@@ -1,11 +1,47 @@
 class Solution {
+    private int[] numsArray;
+    
     public int maxSubArray(int[] nums) {
-        int maxLocal = 0;
-        int maxGlobal = Integer.MIN_VALUE;
-        for(int i = 0; i < nums.length; i++){
-            maxLocal = Math.max(nums[i], maxLocal+nums[i]);
-            maxGlobal = Math.max(maxLocal, maxGlobal);
+        numsArray = nums;
+        
+        // Our helper function is designed to solve this problem for
+        // any array - so just call it using the entire input!
+        return findBestSubarray(0, numsArray.length - 1);
+    }
+    
+    private int findBestSubarray(int left, int right) {
+        // Base case - empty array.
+        if (left > right) {
+            return Integer.MIN_VALUE;
         }
-        return maxGlobal;
+        
+        int mid = Math.floorDiv(left + right, 2);
+        int curr = 0;
+        int bestLeftSum = 0;
+        int bestRightSum = 0;
+        
+        // Iterate from the middle to the beginning.
+        for (int i = mid - 1; i >= left; i--) {
+            curr += numsArray[i];
+            bestLeftSum = Math.max(bestLeftSum, curr);
+        }
+        
+        // Reset curr and iterate from the middle to the end.
+        curr = 0;
+        for (int i = mid + 1; i <= right; i++) {
+            curr += numsArray[i];
+            bestRightSum = Math.max(bestRightSum, curr);
+        }
+        
+        // The bestCombinedSum uses the middle element and the best
+        // possible sum from each half.
+        int bestCombinedSum = numsArray[mid] + bestLeftSum + bestRightSum;
+        
+        // Find the best subarray possible from both halves.
+        int leftHalf = findBestSubarray(left, mid - 1);
+        int rightHalf = findBestSubarray(mid + 1, right);
+        
+        // The largest of the 3 is the answer for any given input array.
+        return Math.max(bestCombinedSum, Math.max(leftHalf, rightHalf));
     }
 }
