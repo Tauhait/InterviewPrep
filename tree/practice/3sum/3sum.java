8b61a6c1f5e58f11d80096bcb5c2501fd2ca4d1c
Tuhin Dutta <dt_tauhait@hotmail.com>
2023-06-09 22:53:31 +0530

    Time: 444 ms (30.59%) | Memory: 50.9 MB (29.11%) - LeetSync

--- tree/practice/3sum/3sum.java
+++ tree/practice/3sum/3sum.java
@@ -1,22 +1,23 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> res = new HashSet<>();
-        Set<Integer> dups = new HashSet<>();
-        Map<Integer, Integer> seen = new HashMap<>();
-        for (int i = 0; i < nums.length; ++i)
-            if (dups.add(nums[i])) {
-                for (int j = i + 1; j < nums.length; ++j) {
-                    int complement = -nums[i] - nums[j];
-                    if (seen.containsKey(complement) && 
-                        seen.get(complement) == i) {
-                        List<Integer> triplet = 
-                            Arrays.asList(nums[i], nums[j], complement);
-                        Collections.sort(triplet);
-                        res.add(triplet);
-                    }
-                    seen.put(nums[j], i);
-                }
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length && nums[i] <= 0; ++i)
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                twoSum(nums, i, res);
             }
-        return new ArrayList(res);
+        return res;
+    }
+    void twoSum(int[] nums, int i, List<List<Integer>> res) {
+        var seen = new HashSet<Integer>();
+        for (int j = i + 1; j < nums.length; ++j) {
+            int complement = -nums[i] - nums[j];
+            if (seen.contains(complement)) {
+                res.add(Arrays.asList(nums[i], nums[j], complement));
+                while (j + 1 < nums.length && nums[j] == nums[j + 1])
+                    ++j;
+            }
+            seen.add(nums[j]);
+        }
     }
 }
