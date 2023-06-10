d2bc504fa2b041332289467b05a05e7752d1b547
Tuhin Dutta <dt_tauhait@hotmail.com>
2023-06-10 21:32:08 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (22.47%) - LeetSync

--- tree/practice/subsets-ii/subsets-ii.java
+++ tree/practice/subsets-ii/subsets-ii.java
@@ -1,25 +1,20 @@
 class Solution { 
     private List<List<Integer>> subsets;
     private void findSubsets(int[] nums, int start, List<Integer> subset, Set<String> uniqueSubsets){
+        subsets.add(new ArrayList<Integer>(subset));
         if(start < nums.length) {
             for(int i = start; i < nums.length; i++){
+                if(i != start && nums[i] == nums[i-1]) continue;
                 subset.add(nums[i]);
                 findSubsets(nums, i+1, subset, uniqueSubsets);
-                String thisSubset = subset.toString();
-                if(!uniqueSubsets.contains(thisSubset)) {
-                    subsets.add(new ArrayList<Integer>(subset));
-                    uniqueSubsets.add(thisSubset);
-                }
                 subset.remove(subset.size()-1);
-            }            
+            }
         }
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         Set<String> uniqueSubsets = new HashSet<>();
         subsets = new ArrayList<List<Integer>>();
-        subsets.add(new ArrayList<Integer>());
-        uniqueSubsets.add((new ArrayList<Integer>()).toString());
         findSubsets(nums, 0, new ArrayList<Integer>(), uniqueSubsets);
         return subsets;
 
