0e55fdbffef568f1d8275a83a172138690d0f7d1
Tuhin Dutta <dt_tauhait@hotmail.com>
2023-06-11 00:26:52 +0530

    Time: 3 ms (66.31%) | Memory: 42.6 MB (73.14%) - LeetSync

---
+++ tree/practice/combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    private List<List<Integer>> combinations;
+    private void findCombinations(int[] nums, int target, 
+                                    int start, List<Integer> subset){
+        if(target == 0){
+            combinations.add(new ArrayList<Integer>(subset));
+        }
+        else if(start < nums.length && nums[start] <= target){
+            for(int i = start; i < nums.length; i++){
+                if( i > start && nums[i] == nums[i-1]) continue;
+                subset.add(nums[i]);
+                findCombinations(nums, target-nums[i], i+1, subset);
+                subset.remove(subset.size()-1);
+            }
+        }
+    }
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        combinations = new ArrayList<List<Integer>>();
+        findCombinations(candidates, target, 0, new ArrayList<Integer>());
+        return combinations;
+    }
+}
