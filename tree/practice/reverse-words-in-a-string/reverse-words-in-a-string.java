f51a59d451aa61dfce360e71ccb91514cc794c70
Tuhin Dutta <dt_tauhait@hotmail.com>
2023-06-18 21:13:21 +0530

    Time: 9 ms (49.67%) | Memory: 42 MB (90.33%) - LeetSync

---
+++ tree/practice/reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+  public String reverseWords(String s) {
+    // remove leading spaces
+    s = s.trim();
+    // split by multiple spaces
+    List<String> wordList = Arrays.asList(s.split("\\s+"));
+    Collections.reverse(wordList);
+    return String.join(" ", wordList);
+  }
+}
