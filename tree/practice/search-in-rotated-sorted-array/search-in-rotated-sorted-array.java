79c111266a6b4a7b9c77c683d710972f7ce94bf3
Tuhin Dutta <dt_tauhait@hotmail.com>
2023-06-18 19:58:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (68.06%) - LeetSync

---
+++ tree/practice/search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while(low <= high){
+            int mid = (low + high) >>> 1;
+            
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] >= nums[low]){ //non-rotated side
+                if(target >= nums[low] && target < nums[mid]) { 
+                    high = mid - 1;
+                }
+                else { 
+                    low = mid + 1;
+                }
+            } else { //rotated side
+                if(target > nums[mid] && target <= nums[high]) { 
+                    low = mid + 1;
+                }
+                else { 
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
