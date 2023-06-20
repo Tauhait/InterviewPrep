6fd756dfe74f2a1f829e17f17a4948dfaee298ce
Tuhin Dutta <dt_tauhait@hotmail.com>
2023-06-20 20:36:59 +0530

    Time: 1 ms (34.58%) | Memory: 40.3 MB (92.52%) - LeetSync

--- tree/practice/find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ tree/practice/find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,47 +1,59 @@
-public class RabinKarp {
-	private int D;
-	private int Q;
-
-	public RabinKarp(int D, int Q) {
-		this.D = D;
-		this.Q = Q;
-	}
-
-	public List<Integer> rabinKarpMatch(String pat, String str) {
-		// your code here
-		int M = pat.length();
-		int N = str.length();
-		ArrayList<Integer> indexes = new ArrayList<Integer>(); // list of starting indexes where
-		long H = ((long)Math.pow(D, M - 1)) % Q;
-		long hashPat = 0;
-		long hashStr = 0;
-		for (int i = 0; i < M; i++) {
-			hashPat = (D * hashPat + pat.charAt(i)) % Q;
-			hashStr = (D * hashStr + str.charAt(i)) % Q;
-		}
-
-		for (int i = 0; i < N; i++) {
-			if (hashStr == hashPat) {
-				String substr = str.substring(i, Math.min(i + M, N));
-				if (substr.equals(pat)) {
-					indexes.add(i);
+public class KMP {
+	private void computeLPSArray(String pat, int[] lps) {
+		int len = 0;
+		int i = 1;
+		int m = pat.length();
+		lps[0] = 0;
+		while (i < m) {
+			if (pat.charAt(i) == pat.charAt(len)) {
+				lps[i] = len + 1;
+				len++;
+				i++;			
+			} else {
+				if (len != 0) {
+					len = lps[len - 1];
+				} else {
+					lps[i] = 0;
+					i++;			
 				}
 			}
-			if (i < N - M) {
-				hashStr = (hashStr + Q - str.charAt(i) * H % Q) % Q;
-				hashStr = (hashStr * D + str.charAt(i + M)) % Q;
+		}
+	}
+
+	public List<Integer> kmpMatch(String pat, String str) {
+		List<Integer> matchPoints = new ArrayList<>();
+		int n = str.length();
+		int m = pat.length();
+		int[] lps = new int[m];
+		computeLPSArray(pat, lps);
+		int i = 0, j = 0;
+		while (i < n) {
+			if (str.charAt(i) == pat.charAt(j)) {
+				i++;
+				j++;
+			} else {
+				if (j != 0) {
+					j = lps[j - 1];
+				} else {
+					i += 1;
+				}
+			}
+			if (j == m) {
+				matchPoints.add(i - j);
+				j = lps[j - 1];
 			}
 		}
-		if (indexes.isEmpty()) {
-			indexes.add(-1);
+		if (matchPoints.isEmpty()) {
+			matchPoints.add(-1);
 		}
-		return indexes;
+		return matchPoints;
 	}
 }
 class Solution {
     public int strStr(String haystack, String needle) {
         if(needle.length() > haystack.length()) return -1;
-        RabinKarp rk = new RabinKarp(26, 997);
-        return rk.rabinKarpMatch(needle, haystack).get(0);
+        // RabinKarp rk = new RabinKarp(26, 997);
+				KMP kmp = new KMP();		
+        return kmp.kmpMatch(needle, haystack).get(0);
     }
 }
