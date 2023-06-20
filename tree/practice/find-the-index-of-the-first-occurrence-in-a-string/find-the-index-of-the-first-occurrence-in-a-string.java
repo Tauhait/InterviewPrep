da8ee4201e5c79fbb3aaf59edb5ce639df9fced4
Tuhin Dutta <dt_tauhait@hotmail.com>
2023-06-20 14:03:07 +0530

    Time: 1 ms (34.58%) | Memory: 40.5 MB (84.31%) - LeetSync

--- tree/practice/find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ tree/practice/find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,44 +1,47 @@
+public class RabinKarp {
+	private int D;
+	private int Q;
+
+	public RabinKarp(int D, int Q) {
+		this.D = D;
+		this.Q = Q;
+	}
+
+	public List<Integer> rabinKarpMatch(String pat, String str) {
+		// your code here
+		int M = pat.length();
+		int N = str.length();
+		ArrayList<Integer> indexes = new ArrayList<Integer>(); // list of starting indexes where
+		long H = ((long)Math.pow(D, M - 1)) % Q;
+		long hashPat = 0;
+		long hashStr = 0;
+		for (int i = 0; i < M; i++) {
+			hashPat = (D * hashPat + pat.charAt(i)) % Q;
+			hashStr = (D * hashStr + str.charAt(i)) % Q;
+		}
+
+		for (int i = 0; i < N; i++) {
+			if (hashStr == hashPat) {
+				String substr = str.substring(i, Math.min(i + M, N));
+				if (substr.equals(pat)) {
+					indexes.add(i);
+				}
+			}
+			if (i < N - M) {
+				hashStr = (hashStr + Q - str.charAt(i) * H % Q) % Q;
+				hashStr = (hashStr * D + str.charAt(i + M)) % Q;
+			}
+		}
+		if (indexes.isEmpty()) {
+			indexes.add(-1);
+		}
+		return indexes;
+	}
+}
 class Solution {
-    private final int D = 26;
-    private final int Q = 997;
-    
-    private List<Integer> rabinKarpMatch(String pat, String str)
-    {
-        // your code here
-        int M = pat.length();
-        int N = str.length();
-        ArrayList<Integer> indexes = new ArrayList<Integer>();
-        long H = ((long)Math.pow(D, M - 1)) % Q;
-        long hashPat = 0;
-        long hashStr = 0;
-        for(int i = 0; i < M; i++) {
-            hashPat = (D * hashPat + pat.charAt(i)) % Q;
-            hashStr = (D * hashStr + str.charAt(i)) % Q;
-        }
-        
-        for(int i = 0; i < N; i++){
-            if(hashStr == hashPat){
-                String substr = str.substring(i, Math.min(i + M, N));
-                if(substr.equals(pat)){
-                    indexes.add(i);
-                }
-            }            
-            if(i < N - M){
-                hashStr = (hashStr + Q - str.charAt(i) * H % Q) % Q;
-                hashStr = (hashStr * D + str.charAt(i + M)) % Q;
-            }
-        }
-        if(indexes.isEmpty()) {
-            indexes.add(-1);
-        }
-        return indexes;
-    }
     public int strStr(String haystack, String needle) {
         if(needle.length() > haystack.length()) return -1;
-        List<Integer> indexes = rabinKarpMatch(needle, haystack);
-        for(int index : indexes){
-            System.out.print(index + " ");
-        }
-        return indexes.get(0);
+        RabinKarp rk = new RabinKarp(26, 997);
+        return rk.rabinKarpMatch(needle, haystack).get(0);
     }
 }
