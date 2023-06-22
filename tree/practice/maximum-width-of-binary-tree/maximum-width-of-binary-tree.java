7d671037f63463d4914e7626626fc3edc96b2004
Tuhin Dutta <dt_tauhait@hotmail.com>
2023-06-23 01:31:00 +0530

    Time: 1 ms (100.00%) | Memory: 43 MB (51.98%) - LeetSync

--- tree/practice/maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ tree/practice/maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -24,32 +24,30 @@ class Pair{
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
         Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(0,root));
-        int max = Integer.MIN_VALUE;
+        q.offer(new Pair(0, root));
+        int width = Integer.MIN_VALUE;
         while(!q.isEmpty()){
             int size = q.size();
+            //to make the id starting from zero, prevent overflow
+            int mmin = q.peek().pos;    
+            
             int start = 0;
             int end = 0;
             for(int i = 0 ; i < size; i ++){
-                Pair p = q.poll();
-                int pos = p.pos;
-                TreeNode node = p.node;
+                int cur_id = q.peek().pos - mmin;
+                TreeNode node = q.poll().node;;
                 
-                if(i == 0){
-                    start = pos;
-                }
-                if(i == size -1){
-                    end = pos;
-                }
+                if(i == 0) start = cur_id;                
+                if(i == size -1) end = cur_id;
                 if(node.left != null){
-                    q.offer(new Pair(2*pos+1,node.left));
+                    q.offer(new Pair(2 * cur_id + 1,node.left));
                 }
                 if(node.right != null){
-                    q.offer(new Pair(2*pos+2,node.right));
+                    q.offer(new Pair(2 * cur_id + 2,node.right));
                 }
             }
-            max = Math.max(max,end+1-start);
+            width = Math.max(width, end - start + 1);
         }
-        return max;
+        return width;
     }
 }
