055b22d10323e04f26a1e5f4df6669689a12af6a
Tuhin Dutta <dt_tauhait@hotmail.com>
2023-06-28 09:45:56 +0530

    Time: 27 ms (29.86%) | Memory: 42 MB (77.91%) - LeetSync

---
+++ tree/practice/clone-graph/clone-graph.java
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node == null) return node;
+        
+        HashMap<Node, Node> visited = new HashMap<Node, Node>();        
+        Queue<Node> bfsQ = new LinkedList<Node>();
+        
+        bfsQ.add(node);
+        visited.put(node, new Node(node.val, new ArrayList<Node>()));
+        
+        while(!bfsQ.isEmpty()){
+            Node q = bfsQ.remove();
+            Node qClone = visited.get(q);
+            for(Node neighbor : q.neighbors){
+                //visit the neighbor and make its clone
+                if(!visited.containsKey(neighbor)){
+                    
+                    bfsQ.add(neighbor);
+                    visited.put(neighbor, new Node(neighbor.val, new ArrayList<Node>()));                    
+                }
+                // Add the clone of the neighbor to the neighbors of the clone node "q".
+                Node neighborClone = visited.get(neighbor);                
+                qClone.neighbors.add(neighborClone);
+            }
+        }
+        return visited.get(node);
+    }
+}
