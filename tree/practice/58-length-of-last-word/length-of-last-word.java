3d188c1f7eb3c0ca4fa4f30d43413f32cbde3b77
Tuhin Dutta <dt_tauhait@hotmail.com>
2023-09-03 18:01:51 +0530

    Time: 1 ms (44.34%) | Memory: 41.2 MB (8.53%) - LeetSync

---
+++ tree/practice/58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        String[] words = s.split(" ");
+        return words[words.length - 1].length();
+    }
+}
